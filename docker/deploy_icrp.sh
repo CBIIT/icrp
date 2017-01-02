c97edb630c94398578af6664c44ff1eee3038ec3
park-brian <re.public@outlook.com>
2017-01-01 19:10:17 -0500

    updated deployment script

--- docker/deploy_icrp.sh
+++ docker/deploy_icrp.sh
@@ -8,7 +8,7 @@ temp_folder="$icrp_root/.tmp"
 
 ## docker
 docker_root="$icrp_root/docker"
-docker_compose_file="$icrp_root/docker/docker-compose.yml"
+docker_compose_file="$docker_root/docker-compose.yml"
 web_container="icrp-web-container"
 mysql_container="icrp-mysql-container"
 
@@ -18,6 +18,7 @@ drupal_filename="drupal-$drupal_version.tar.gz"
 drupal_filepath="$temp_folder/$drupal_filename"
 drupal_url="https://ftp.drupal.org/files/projects/$drupal_filename"
 drupal_root="$docker_root/drupal"
+drupal_custom_modules_root="$drupal_root/modules/custom"
 
 start_timestamp=$(date +%s)
 
@@ -42,7 +43,9 @@ fi
 ## clone repository if it does not exist, otherwise pull latest changes
 echo "Updating repository..."
 if [ -d "$repository_root" ]; then
-  git -C "$repository_root" pull 
+  pushd "$repository_root"
+  git pull
+  popd
 else
   git clone "$repository_url" "$repository_root" 
   echo
@@ -59,7 +62,7 @@ echo
 
 ## extract drupal release
 echo -e "Extracting drupal root...\n"
-rm -rf "$drupal_root"
+sudo rm -rf "$drupal_root"
 mkdir -p "$drupal_root"
 tar xf "$drupal_filepath" --strip-components 1 --directory "$drupal_root"
 
@@ -75,40 +78,66 @@ done
 echo
 
 ## copy icrp-specific configuration files
+## these should be located in a 'configuration' directory
+## in the icrp root deployment directory
 if [ -d "$configuration_directory" ]; then
   echo -e "Copying configuration files...\n"
   if [ -f "$configuration_directory/settings.php" ]; then
     cp "$configuration_directory/settings.php" "$drupal_root/sites/default/"
+  fi
+
+  if [ -f "$configuration_directory/connection.ini" ]; then
+    for directory in "$drupal_custom_modules_root/db_project_view" "$drupal_custom_modules_root/db_search_api"
+    do
+      if [ -d "$directory/src/Controller" ]; then
+        echo "Copying connection settings to $directory/src/Controller"
+        cp "$configuration_directory/connection.ini" "$directory/src/Controller"
+      fi
+    done
   fi
 else
   mkdir -p "$configuration_directory"
 fi
 
 
+echo -e "Initializing docker directory...\n"
+mkdir -p "$docker_root"
+cp -R "$repository_root/docker/build/"* "$docker_root/"
+
+
 ## copy icrp database dump to docker build context
 if [ -f "$repository_root/icrp.sql" ]; then
   echo -e "Copying icrp database dump to docker directory...\n"
-  cp "$repository_root/icrp.sql" "$docker_root"
+  cp "$repository_root/icrp.sql" "$docker_root/"
 fi
 
-echo
 
 ## restart container
-echo -e "Restarting docker containers...\n"
-docker-compose -f $docker_compose_file down
-nohup docker-compose -f $docker_compose_file up > $temp_folder/log 2> $temp_folder/error_log < /dev/null &
+pushd $docker_root
+echo -e "Removing containers..."
+docker-compose down
+
+
+echo -e "Starting containers...\n"
+rm -rf $temp_folder/log $temp_folder/error_log
+nohup docker-compose up > $temp_folder/log 2> $temp_folder/error_log < /dev/null &
+popd
 
 ## wait until mysql container is ready
-while [tail -n1 $temp_folder/log | grep -v "mysqld.sock" ]; do sleep 2; done
+## while [ $(tail -n1 $temp_folder/log | grep -v "mysqld.sock") ]; do sleep 2; done
+echo "sleeping"
+sleep 10
 
 ## import drupal database
 echo -e "Importing icrp database...\n"
-docker exec $mysql_container bash -c "mysqladmin -udrupal -pdrupal drop drupal"
-docker exec $mysql_container bash -c "mysqladmin -udrupal -pdrupal create drupal"
+docker exec $mysql_container bash -c "mysql -udrupal -pdrupal -e 'drop database drupal'"
+docker exec $mysql_container bash -c "mysql -udrupal -pdrupal -e 'create database drupal'"
 docker exec $mysql_container bash -c "mysql -udrupal -pdrupal drupal < /tmp/icrp.sql"
 
 ## rebuild drupal cache
 echo -e "Rebuilding cache...\n"
 docker exec $web_container bash -c "cd /var/www/html && drush cr"
 
-echo -e "Done in $($(date +%s)-$start_timestamp)s\n"
+
+
+echo -e "Done\n"
