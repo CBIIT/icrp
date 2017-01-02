5d727a3d3c4c130fc6ced10c3c46b75030f8865e
park-brian <re.public@outlook.com>
2017-01-01 22:50:00 -0500

    consolidating functions in db_search

--- modules/custom/db_search_api/src/Controller/DatabaseSearchApiController.php
+++ modules/custom/db_search_api/src/Controller/DatabaseSearchApiController.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\db_search_api\Controller\DbSearchAPIController.
+ * Contains \Drupal\db_search_api\Controller\DatabaseSearchAPIController.
  */
 
 namespace Drupal\db_search_api\Controller;
@@ -17,7 +17,7 @@ use PDO;
 /**
  * Controller routines for db_search_api routes.
  */
-class DbSearchApiController extends ControllerBase {
+class DatabaseSearchAPIController extends ControllerBase {
 
   private static $connection_ini = "connection.ini";
 
@@ -94,45 +94,28 @@ class DbSearchApiController extends ControllerBase {
     );
   }
 
-
   /**
-   * Parses user input 
-   * @param $input - An associative array containing query parameters 
-   *
-   *  page_size
-   *  page_number
-   *  sort_column
-   *  sort_type
-   *  search_terms
-   *  search_type
-   *  year
-   *  institution
-   *  pi_first_name
-   *  pi_last_name
-   *  pi_orcid
-   *  award_code
-   *  countries
-   *  states
-   *  cities
-   *  funding_organizations
-   *  cancer_types
-   *  project_types
-   *  cso_codes
-   *
-   * @return An array containing mapped parameters
-   */  
-  function parse_input($input) {
-    $parameter_mappings = self::$parameter_mappings;
-    $query_param = array();
+  * Creates a PDO prepared statement for searching projects
+  * @param $pdo - The PDO connection oject
+  * @param $parameters - An associative array containing 
+  * parameters to bind to the statement (from parse_input())
+  */
+  function create_prepared_statement($pdo, $parameters) {
+    $stmt_conditions = array();
+    $stmt_parameters = array();
 
-    foreach (array_keys($input) as $key) {
-      if (array_key_exists($key, $parameter_mappings)) {
-        $query_key = $parameter_mappings[$key];
-        $query_param[$query_key] = $input[$key];
-      }
+    foreach (array_keys($parameters) as $key) {
+      $query_key = ":".$key;
+      $param_key = "@".$key;
+
+      $stmt_parameters[$query_key] = $parameters[$key];
+      array_push($stmt_conditions, $param_key."=".$query_key);
     }
 
-    return $query_param;
+    $stmt = $pdo->prepare("SET NOCOUNT ON; EXECUTE GetProjects " . implode(",", $stmt_conditions));
+    self::bind_parameters($stmt, $stmt_parameters);
+
+    return $stmt;
   }
 
   /**
@@ -154,26 +137,46 @@ class DbSearchApiController extends ControllerBase {
       }
     }
   }
+  
+  /**
+  * Queries the database for projects
+  * @param $parameters - An associative array of parameters
+  */
+  function search_database($parameters) {
+    $pdo = self::get_connection();
+    $stmt = self::create_prepared_statement($pdo, $parameters);
+    if ($stmt->execute()) {
+      $rows = array();
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
-  function create_prepared_statement($pdo, $parameters) {
-    $stmt_conditions = array();
-    $stmt_parameters = array();
+        $result = array(
+          'project_id' => $row['ProjectID'],
+          'project_title' => $row['Title'],
+          'pi_name' => $row['piLastName'].", ".$row['piFirstName'],
+          'institution' => $row['institution'],
+          'city' => $row['City'],
+          'state' => $row['State'],
+          'country' => $row['country'],
+          'funding_organization' => $row['FundingOrg'],
+          'award_code' => $row['AwardCode']
+        );
 
-    foreach (array_keys($parameters) as $key) {
-      $query_key = ":".$key;
-      $param_key = "@".$key;
+        array_push($rows, $result);
+      }
 
-      $stmt_parameters[$query_key] = $parameters[$key];
-      array_push($stmt_conditions, $param_key."=".$query_key);
+      return $rows;
     }
 
-    $stmt = $pdo->prepare("SET NOCOUNT ON; EXECUTE GetProjects " . implode(",", $stmt_conditions));
-    self::bind_parameters($stmt, $stmt_parameters);
-
-    return $stmt;
+    return array("No results found");
   }
 
+  /**
+  * Aggregates unique values from each column
+  * @param $parameters - An associative array containing 
+  * search parameters  (from parse_input())
+  */
   function count_database_groups($parameters) {
+    $pdo = self::get_connection();
 
     $groups = array(
       'count' => 0,
@@ -182,8 +185,6 @@ class DbSearchApiController extends ControllerBase {
       'projects_by_cancer_type' => array(),
       'projects_by_type' => array()
     );
-
-    $pdo = self::get_connection();
 
     foreach (array('PageSize', 'PageNumber', 'SortCol', 'SortDirection') as $key) {
       if (isset($parameters[$key])) {
@@ -194,7 +195,6 @@ class DbSearchApiController extends ControllerBase {
     $stmt = self::create_prepared_statement($pdo, $parameters);
     if ($stmt->execute()) {
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
 
         $country = $row['country'];
         $cso_category = $row['CSOCode'][0];
@@ -233,52 +233,12 @@ class DbSearchApiController extends ControllerBase {
     return $groups;
   }
 
-  /**
-  * Queries the database for projects
-  * @param $parameters - An associative array of parameters
-  */
-  function query_db($parameters) {
-    $pdo = self::get_connection();
-    $stmt = self::create_prepared_statement($pdo, $parameters);
-    if ($stmt->execute()) {
-      $rows = array();
-      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
-        $result = array(
-          'project_id' => $row['ProjectID'],
-          'project_title' => $row['Title'],
-          'pi_name' => $row['piLastName'].", ".$row['piFirstName'],
-          'institution' => $row['institution'],
-          'city' => $row['City'],
-          'state' => $row['State'],
-          'country' => $row['country'],
-          'funding_organization' => $row['FundingOrg'],
-          'award_code' => $row['AwardCode']
-        );
-
-        array_push($rows, $result);
-      }
-
-      return $rows;
-    }
-
-    return array("No results found");
-  }
-
-  function query_search($json) {
-    return self::query_db(self::parse_input($json));
-  }
-
-  function query_group($json) {
-    return self::count_database_groups(self::parse_input($json));
-  }
-
 
 
   /**
   * Adds CORS Headers to a response
   */
-  public function addCorsHeaders($response) {
+  public function add_cors_headers($response) {
     $response->headers->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
     $response->headers->set('Access-Control-Allow-Origin', '*');
     $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
@@ -287,10 +247,10 @@ class DbSearchApiController extends ControllerBase {
   }
 
   /**
-  * Extracts valid fields from a request object 
+  * Extracts valid fields from a request object
+  * and maps them to sql parameters
   */
-  public function getFields(Request $request) {
-
+  public function get_fields(Request $request) {
     $fields = array();
     foreach(array(
       'page_size',
@@ -313,55 +273,63 @@ class DbSearchApiController extends ControllerBase {
       'project_types',
       'cso_codes'
     ) as $field) {
-
       $value = $request->query->get($field);
-
       if ($value) {
-        $fields[$field] = $value;  
+
+        // retrieve the mapped field value
+        $mapped_field = self::$parameter_mappings[$field];
+        
+        // apply mapping to values in sort_column
+        if ($field == 'sort_column') {
+            $mapped_value = self::$sort_column_mappings[$value];
+            if ($mapped_value) {
+                $fields[$mapped_field] = $mapped_value;
+            }
+        }
+
+        // do not modify values in other fields
+        elseif ($mapped_field) {
+            $fields[$mapped_field] = $value;
+        }
       }
     }
 
     return $fields;
   }
 
-  public function searchDatabase($param) {
-    return self::query_search($param);
-  }
-
-  public function countGroups($param) {
-    return self::query_group($param);
-  }
-
   /**
    * Callback for `db/public/search/` API method.
    */
-  public function publicSearch( Request $request ) {
-    $param = self::getFields($request);
-    $response = new JSONResponse( self::searchDatabase($param) );
-    return self::addCorsHeaders($response);
+  public function public_search( Request $request ) {
+    $param = self::get_fields($request);
+    $response = new JSONResponse( self::search_database($param) );
+    return self::add_cors_headers($response);
   }
 
-  public function publicAnalytics( Request $request ) {
-    $param = self::getFields($request);
-    $response = new JSONResponse( self::countGroups($param) );
-    return self::addCorsHeaders($response);
+  /**
+   * Callback for `db/public/analytics/` API method.
+   */
+  public function public_analytics( Request $request ) {
+    $param = self::get_fields($request);
+    $response = new JSONResponse( self::count_database_groups($param) );
+    return self::add_cors_headers($response);
   }
 
   /**
    * Callback for `db/partner/search/` API method.
    */
-  public function partnerSearch( Request $request ) {
-    $param = self::getFields($request);
-    $response = new JSONResponse( self::searchDatabase($param) );
-    return self::addCorsHeaders($response);
+  public function partner_search( Request $request ) {
+    $param = self::get_fields($request);
+    $response = new JSONResponse( self::search_database($param) );
+    return self::add_cors_headers($response);
   }
 
   /**
    * Callback for `db/partner/analytics/` API method.
    */
-  public function partnerAnalytics( Request $request, $type ) {
-    $param = self::getFields($request);
-    $response = new JSONResponse( self::countGroups($param) );
-    return self::addCorsHeaders($response);
+  public function partner_analytics( Request $request, $type ) {
+    $param = self::get_fields($request);
+    $response = new JSONResponse( self::count_database_groups($param) );
+    return self::add_cors_headers($response);
   }
 }
