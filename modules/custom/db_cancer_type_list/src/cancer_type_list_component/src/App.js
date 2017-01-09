e34e18ea7070d41a34f01cd2f37720643c9cfdd3
park-brian <re.public@outlook.com>
2017-01-09 17:14:02 -0500

    building assets for db_cancer_type_list

--- modules/custom/db_cancer_type_list/src/cancer_type_list_component/src/App.js
+++ modules/custom/db_cancer_type_list/src/cancer_type_list_component/src/App.js
@@ -3,17 +3,42 @@ import DataTable from './DataTable';
 import './App.css';
 
 class App extends Component {
-  data = [];
-  columns = [];
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      columns: []
+    }
+    this.getCancerTypes();
+  }
 
   getCancerTypes() {
+    fetch('https://icrpartnership-demo.org/partners/cancer-type-list/get')
+    .then(response => response.json())
+    .then(response => {
+
+      this.setState({
+        data: response, 
+        columns: [
+          {
+            label: 'Cancer Type',
+            value: 'label',
+            link: 'url'
+          },
+          {
+            label: 'Additional Details',
+            value: 'description'
+          }
+        ]
+      })
+    })
     return ;
   }
 
   render() {
     return (
       <div className="responsive">
-        <DataTable columns={this.columns} data={this.data} />
+        <DataTable columns={this.state.columns} data={this.state.data} />
       </div>
     );
   }
