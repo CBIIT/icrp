16341eff04d6d349a21f7b421f22024335aab2a4
park-brian <re.public@outlook.com>
2017-01-17 13:09:13 -0500

    updated project view

---
+++ modules/custom/db_project_view/src/project-view-component/src/DataTable.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './DataTable.css'
+
+class DataTable extends Component {
+
+  state = {
+    collapsed: true
+  };
+
+
+
+  /** @type {any[]} */
+  data = [];
+
+  /** @type {{"label": string, "value": string|number, "link"?: string}[]} */
+  columns = [];
+
+  render() {
+    return (
+      <div className="data-table-wrapper">
+        <table className="data-table">
+          <thead>
+            <tr>
+              {this.props.columns.map((column, columnIndex) => 
+                <th key={columnIndex}>{column.label}</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map((row, rowIndex) =>
+              ((this.state.collapsed 
+                && rowIndex < this.props.limit) 
+                || !this.state.collapsed) 
+              &&
+              <tr key={rowIndex}>
+                {this.props.columns.map((column, columnIndex) => 
+                  <td key={columnIndex}>
+                    { (row[column.link]
+                      ? <a href={row[column.link]} target="_blank">
+                          {row[column.value]}
+                        </a>
+                      : row[column.value]) || 'Not Specified'}
+                  </td>
+                )}
+              </tr>)}
+          </tbody>
+        </table>
+
+        <div className="text-center">
+          <button className="btn btn-default"
+            onClick={e => this.setState({collapsed: !this.state.collapsed})}>
+            {this.state.collapsed
+            ? 'Show All Projects'
+            : 'Limit to Top 5 Projects'}
+          </button>
+        </div>  
+      </div>
+    );
+  }    
+}
+
+export default DataTable;
