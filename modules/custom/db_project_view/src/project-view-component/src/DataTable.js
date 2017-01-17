d04ca66c8a6c741078b6287a0d53bca1ae9ad928
park-brian <re.public@outlook.com>
2017-01-17 14:07:25 -0500

    rebuilt assets

--- modules/custom/db_project_view/src/project-view-component/src/DataTable.js
+++ modules/custom/db_project_view/src/project-view-component/src/DataTable.js
@@ -17,37 +17,39 @@ class DataTable extends Component {
 
   render() {
     return (
-      <div className="data-table-wrapper">
-        <table className="data-table">
-          <thead>
-            <tr>
-              {this.props.columns.map((column, columnIndex) => 
-                <th key={columnIndex}>{column.label}</th>
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.data.map((row, rowIndex) =>
-              ((this.state.collapsed 
-                && rowIndex < this.props.limit) 
-                || !this.state.collapsed) 
-              &&
-              <tr key={rowIndex}>
+      <div>
+        <div className="table-responsive">
+          <table className="data-table">
+            <thead>
+              <tr>
                 {this.props.columns.map((column, columnIndex) => 
-                  <td key={columnIndex}>
-                    { (row[column.link]
-                      ? <a href={row[column.link]} target="_blank">
-                          {row[column.value]}
-                        </a>
-                      : row[column.value]) || 'Not Specified'}
-                  </td>
+                  <th key={columnIndex}>{column.label}</th>
                 )}
-              </tr>)}
-          </tbody>
-        </table>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.data.map((row, rowIndex) =>
+                ((this.state.collapsed 
+                  && rowIndex < this.props.limit) 
+                  || !this.state.collapsed) 
+                &&
+                <tr key={rowIndex}>
+                  {this.props.columns.map((column, columnIndex) => 
+                    <td key={columnIndex}>
+                      { (row[column.link]
+                        ? <a href={row[column.link]} target="_blank">
+                            {row[column.value]}
+                          </a>
+                        : row[column.value]) || 'Not Specified'}
+                    </td>
+                  )}
+                </tr>)}
+            </tbody>
+          </table>
+        </div>
         
         {this.props.data.length > this.props.limit &&
-        <div className="text-center">
+        <div className="text-center margin-top">
           <button className="btn btn-default"
             onClick={e => this.setState({collapsed: !this.state.collapsed})}>
             {this.state.collapsed
