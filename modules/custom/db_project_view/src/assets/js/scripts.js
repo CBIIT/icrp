c0d327a7f456af5bca9a3a4c12c8d634a1291075
park-brian <re.public@outlook.com>
2017-01-17 13:18:56 -0500

    updated js snippet

--- modules/custom/db_project_view/src/assets/js/scripts.js
+++ modules/custom/db_project_view/src/assets/js/scripts.js
@@ -1,1 +1,3 @@
-console.log('loaded');
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+}
