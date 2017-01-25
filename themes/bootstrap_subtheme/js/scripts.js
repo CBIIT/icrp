6ee4cebb2a8c40cbccc8bb8e924c487869d9e0b3
park-brian <re.public@outlook.com>
2017-01-25 15:26:35 -0500

    adding script to enable resizable table columns

--- themes/bootstrap_subtheme/js/scripts.js
+++ themes/bootstrap_subtheme/js/scripts.js
@@ -1,8 +1,16 @@
 (function ($) {
-    Drupal.behaviors.myModuleBehavior = {
-        attach: function (context, settings) {
-        	//$('h2').css('color', 'red');
-        	$("#edit-keys").attr("placeholder", "Search Website");
-        }
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context, settings) {
+      //$('h2').css('color', 'red');
+      $("#edit-keys").attr("placeholder", "Search Website");
     }
+  }
 })(jQuery);
+
+(function ($) {
+  window.enableResizableTableColumns = window.enableResizableTableColumns || function() {
+    $('table').resizableColumns();
+  }
+  window.enableResizableTableColumns();
+})(jQuery);
+
