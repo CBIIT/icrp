a0f72f4d8159e891ce6d14d152ca4b0a3ccc6d93
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-01-26 17:16:26 -0500

    Added twitter feed for client comment, added links to front page

---
+++ sites/default/files/js/js_0caTeC5eVygV9HjnRHKsqqBm7aLL53G_tBAXmvk9lLM.js
@@ -0,0 +1,190 @@
+(function ($) {
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context, settings) {
+      //$('h2').css('color', 'red');
+      $("#edit-keys").attr("placeholder", "Search Website");
+    }
+  }
+})(window.jQuery);
+
+window['enableResizableTableColumns'] = function() {
+  window.setTimeout(function() {
+    $('table:visible').resizableColumns();
+  }, 0)
+}
+
+window['enableTooltips'] = function() {
+  window.setTimeout(function() {
+    $('[data-toggle="tooltip"]').tooltip({container: 'body'}); 
+  }, 0)
+}
+
+$(document).ready(function() {
+  window.enableResizableTableColumns();
+  window.enableTooltips();
+});;
+!function(t,a,e){"use strict";a.extlink=a.extlink||{},a.extlink.attach=function(e,n){if(n.data.hasOwnProperty("extlink")){var l,i=/^(([^\/:]+?\.)*)([^\.:]{4,})((\.[a-z]{1,4})*)(:[0-9]{1,5})?$/,s=window.location.host.replace(i,"$3$4"),x=window.location.host.replace(i,"$1");l=n.data.extlink.extSubdomains?"([^/]*\\.)?":"www."===x||""===x?"(www\\.)?":x.replace(".","\\.");var p=new RegExp("^https?://"+l+s,"i"),d=!1;n.data.extlink.extInclude&&(d=new RegExp(n.data.extlink.extInclude.replace(/\\/,"\\"),"i"));var r=!1;n.data.extlink.extExclude&&(r=new RegExp(n.data.extlink.extExclude.replace(/\\/,"\\"),"i"));var k=!1;n.data.extlink.extCssExclude&&(k=n.data.extlink.extCssExclude);var c=!1;n.data.extlink.extCssExplicit&&(c=n.data.extlink.extCssExplicit);var o=[],h=[];t("a:not(."+n.data.extlink.extClass+", ."+n.data.extlink.mailtoClass+"), area:not(."+n.data.extlink.extClass+", ."+n.data.extlink.mailtoClass+")",e).each(function(a){try{var e=this.href.toLowerCase();0!==e.indexOf("http")||(e.match(p)||r&&e.match(r))&&(!d||!e.match(d))||k&&t(this).parents(k).length>0||c&&t(this).parents(c).length<1?"AREA"===this.tagName||0!==e.indexOf("mailto:")||k&&t(this).parents(k).length>0||c&&t(this).parents(c).length<1||h.push(this):o.push(this)}catch(n){return!1}}),n.data.extlink.extClass&&a.extlink.applyClassAndSpan(o,n.data.extlink.extClass),n.data.extlink.mailtoClass&&a.extlink.applyClassAndSpan(h,n.data.extlink.mailtoClass),n.data.extlink.extTarget&&n.data.extlink.extTarget&&t(o).attr({target:'_blank', rel:'nofollow'}),a.extlink=a.extlink||{},a.extlink.popupClickHandler=a.extlink.popupClickHandler||function(){return n.data.extlink.extAlert?confirm(n.data.extlink.extAlertText):void 0},t(o).click(function(t){return a.extlink.popupClickHandler(t)})}},a.extlink.applyClassAndSpan=function(a,n){var l;if(e.data.extlink.extImgClass)l=t(a);else{var i=t(a).find("img").parents("a");l=t(a).not(i)}l.addClass(n);var s,x=l.length;for(s=0;x>s;s++){var p=t(l[s]);("inline"===p.css("display")||"inline-block"===p.css("display"))&&(n===e.data.extlink.mailtoClass?p.append('<span class="'+n+'"><span class="element-invisible"> '+e.data.extlink.mailtoLabel+"</span></span>"):p.append('<span class="'+n+'"><span class="element-invisible"> '+e.data.extlink.extLabel+"</span></span>"))}},a.behaviors.extlink=a.behaviors.extlink||{},a.behaviors.extlink.attach=function(t,e){"function"==typeof extlinkAttach?extlinkAttach(t):a.extlink.attach(t,e)}}(jQuery,Drupal,drupalSettings);
+;
+/**
+ * @file
+ * Bootstrap Popovers.
+ */
+
+var Drupal = Drupal || {};
+
+(function ($, Drupal, Bootstrap) {
+  "use strict";
+
+  /**
+   * Extend the Bootstrap Popover plugin constructor class.
+   */
+  Bootstrap.extendPlugin('popover', function (settings) {
+    return {
+      DEFAULTS: {
+        animation: !!settings.popover_animation,
+        html: !!settings.popover_html,
+        placement: settings.popover_placement,
+        selector: settings.popover_selector,
+        trigger: settings.popover_trigger,
+        triggerAutoclose: !!settings.popover_trigger_autoclose,
+        title: settings.popover_title,
+        content: settings.popover_content,
+        delay: parseInt(settings.popover_delay, 10),
+        container: settings.popover_container
+      }
+    };
+  });
+
+  /**
+   * Bootstrap Popovers.
+   *
+   * @todo This should really be properly delegated if selector option is set.
+   */
+  Drupal.behaviors.bootstrapPopovers = {
+    attach: function (context) {
+
+      // Popover autoclose.
+      if ($.fn.popover.Constructor.DEFAULTS.triggerAutoclose) {
+        var $currentPopover = null;
+        $(document)
+          .on('show.bs.popover', '[data-toggle=popover]', function () {
+            var $trigger = $(this);
+            var popover = $trigger.data('bs.popover');
+
+            // Only keep track of clicked triggers that we're manually handling.
+            if (popover.options.originalTrigger === 'click') {
+              if ($currentPopover && !$currentPopover.is($trigger)) {
+                $currentPopover.popover('hide');
+              }
+              $currentPopover = $trigger;
+            }
+          })
+          .on('click', function (e) {
+            var $target = $(e.target);
+            var popover = $target.is('[data-toggle=popover]') && $target.data('bs.popover');
+            if ($currentPopover && !$target.is('[data-toggle=popover]') && !$target.closest('.popover.in')[0]) {
+              $currentPopover.popover('hide');
+              $currentPopover = null;
+            }
+          })
+        ;
+      }
+
+      var elements = $(context).find('[data-toggle=popover]').toArray();
+      for (var i = 0; i < elements.length; i++) {
+        var $element = $(elements[i]);
+        var options = $.extend({}, $.fn.popover.Constructor.DEFAULTS, $element.data());
+
+        // Store the original trigger.
+        options.originalTrigger = options.trigger;
+
+        // If the trigger is "click", then we'll handle it manually here.
+        if (options.trigger === 'click') {
+          options.trigger = 'manual';
+        }
+
+        // Retrieve content from a target element.
+        var $target = $(options.target || $element.is('a[href^="#"]') && $element.attr('href')).clone();
+        if (!options.content && $target[0]) {
+          $target.removeClass('visually-hidden hidden').removeAttr('aria-hidden');
+          options.content = $target.wrap('<div/>').parent()[options.html ? 'html' : 'text']() || '';
+        }
+
+        // Initialize the popover.
+        $element.popover(options);
+
+        // Handle clicks manually.
+        if (options.originalTrigger === 'click') {
+          // To ensure the element is bound multiple times, remove any
+          // previously set event handler before adding another one.
+          $element
+            .off('click.drupal.bootstrap.popover')
+            .on('click.drupal.bootstrap.popover', function (e) {
+              $(this).popover('toggle');
+              e.preventDefault();
+              e.stopPropagation();
+            })
+          ;
+        }
+      }
+    },
+    detach: function (context) {
+      // Destroy all popovers.
+      $(context).find('[data-toggle="popover"]')
+        .off('click.drupal.bootstrap.popover')
+        .popover('destroy')
+      ;
+    }
+  };
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
+;
+/**
+ * @file
+ * Bootstrap Tooltips.
+ */
+
+var Drupal = Drupal || {};
+
+(function ($, Drupal, Bootstrap) {
+  "use strict";
+
+  /**
+   * Extend the Bootstrap Tooltip plugin constructor class.
+   */
+  Bootstrap.extendPlugin('tooltip', function (settings) {
+    return {
+      DEFAULTS: {
+        animation: !!settings.tooltip_animation,
+        html: !!settings.tooltip_html,
+        placement: settings.tooltip_placement,
+        selector: settings.tooltip_selector,
+        trigger: settings.tooltip_trigger,
+        delay: parseInt(settings.tooltip_delay, 10),
+        container: settings.tooltip_container
+      }
+    };
+  });
+
+  /**
+   * Bootstrap Tooltips.
+   *
+   * @todo This should really be properly delegated if selector option is set.
+   */
+  Drupal.behaviors.bootstrapTooltips = {
+    attach: function (context) {
+      var elements = $(context).find('[data-toggle="tooltip"]').toArray();
+      for (var i = 0; i < elements.length; i++) {
+        var $element = $(elements[i]);
+        var options = $.extend({}, $.fn.tooltip.Constructor.DEFAULTS, $element.data());
+        $element.tooltip(options);
+      }
+    },
+    detach: function (context) {
+      // Destroy all tooltips.
+      $(context).find('[data-toggle="tooltip"]').tooltip('destroy');
+    }
+  };
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
+;
