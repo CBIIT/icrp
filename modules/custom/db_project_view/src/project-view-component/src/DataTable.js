aa42768961076d7991ac3e97378cc8f75ba44ba8
park-brian <re.public@outlook.com>
2017-01-27 17:11:37 -0500

    updated project-view-component

--- modules/custom/db_project_view/src/project-view-component/src/DataTable.js
+++ modules/custom/db_project_view/src/project-view-component/src/DataTable.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class DataTable extends Component {
 
   state = {
     collapsed: true
-  };
-
-  refs = {
-
   };
 
   /** @type {any[]} */
@@ -35,9 +33,14 @@ class DataTable extends Component {
     }
 
     // initialize resize overlay
-    let tableResizeOverlay = document.createElement('div');
+    let tableResizeOverlay = document.getElementById('table-resize-overlay') ||
+      document.createElement('div');
+
+    tableResizeOverlay.innerHTML = '';
+    tableResizeOverlay.id = 'table-resize-overlay';
     tableResizeOverlay.style.position = 'relative';
     tableResizeOverlay.style.width = `${state.width}px`;
+
     table.style.width = `${state.width}px`;
 
     table.parentElement.insertBefore(tableResizeOverlay, table);
@@ -45,8 +48,65 @@ class DataTable extends Component {
     // populate overlay div with resize handles
     let headerRow = table.tHead.children[0];
 
-    for (var i = 0; i < headerRow.children.length; ++i) {
+    let resetHandlePositions = () => {
 
+      tableResizeOverlay.style.width = `${state.width}px`
+
+      for (let j = 0; j < state.handles.length; j++) {
+        let header = headerRow.children[j];
+        let handleOffset = header.offsetLeft + header.clientWidth;
+        state.handles[j].style.left = `${handleOffset}px`;
+        state.handles[j].style.height = `${state.height}px`;
+      }
+    }
+
+    // mousemove events will resize table headers
+    let startResizeEvent = e => {
+      e.preventDefault();
+
+      let handle = e.target;
+      state.resizing = true;
+      state.initial.cursorOffset = e.pageX;
+      state.initial.tableWidth = table.clientWidth;
+      state.initial.columnIndex = handle.dataset.index;
+      state.initial.cellWidth = headerRow.children[handle.dataset.index].clientWidth;
+      state.initial.handleOffsets = state.handles.map(handle => handle.offsetLeft)
+    }
+
+    // mousedown events will start the resize event
+    let handleResizeEvent = e => {
+      if (state.resizing) {
+        let index = state.initial.columnIndex;
+        let offset = e.pageX - state.initial.cursorOffset;
+
+        let cell = headerRow.children[index];
+        cell.style.width = `${state.initial.cellWidth + offset}px`
+
+        let width = state.initial.tableWidth + offset;
+        state.width = `${width}px`
+        table.style.width = `${width}px`
+        tableResizeOverlay.style.width = `${width}px`
+        resetHandlePositions();
+      }
+    }
+
+    // mouseup events will stop resizing
+    let endResizeEvent = e => {
+      e.preventDefault();
+
+      if (state.resizing) {
+        state.resizing = false;
+
+        for (let th of headerRow.children) {
+          console.log(th);
+          th.style.width = `${th.clientWidth + 1}px`;
+        }
+
+        resetHandlePositions();
+      }
+    }
+
+    for (var i = 0; i < headerRow.children.length; ++i) {
       let th = headerRow.children[i];
       th.style.width = `${th.clientWidth}px`;
 
@@ -56,59 +116,16 @@ class DataTable extends Component {
       handle.style.left = `${th.offsetLeft + th.clientWidth}px`;
       handle.style.height = `${state.height}px`;
       handle.style.width = '7px';
-      
+
       handle.style.cursor = 'ew-resize';
       handle.style.marginLeft = '-3px';
       handle.style.zIndex = '2';
       handle.dataset.index = i;
       state.handles.push(handle);
 
-      // mousedown events will start the resize event
-      handle.onmousedown = e => {
-        state.resizing = true;
-        state.initial.cursorOffset = e.pageX;
-        state.initial.tableWidth = table.clientWidth;        
-        state.initial.cellWidth = th.clientWidth;
-        state.initial.columnIndex = handle.dataset.index;
-        state.initial.handleOffsets = state.handles.map(handle => handle.offsetLeft)
-
-        table.style.userSelect = 'none';
-        table.style.webkitUserSelect = 'none';
-        table.style.MozUserSelect = 'none';
-        table.setAttribute('unselectable', 'on')
-      };
-
-      // mousemove events will resize table headers
-      document.onmousemove = e => {
-        if (state.resizing) {
-          let index = state.initial.columnIndex;
-          let offset = e.pageX - state.initial.cursorOffset;
-
-          let cell = headerRow.children[index];
-          cell.style.width = `${state.initial.cellWidth + offset}px`
-
-          let width = state.initial.tableWidth + offset;
-          state.width = `${width}px`
-          table.style.width = `${width}px`
-          tableResizeOverlay.style.width = `${width}px`
-
-          for (let j = 0; j < state.handles.length; j ++) {
-            let header = headerRow.children[j];
-            let handleOffset = header.offsetLeft + header.clientWidth;
-            state.handles[j].style.left = `${handleOffset}px`;
-          }
-        }
-      }
-
-      // mouseup events will stop resizing
-      document.onmouseup = e => {
-        state.resizing = false;
-        
-        table.style.userSelect = 'text';
-        table.style.webkitUserSelect = 'text';
-        table.style.MozUserSelect = 'text';
-        table.setAttribute('unselectable', 'off')
-      }
+      handle.onmousedown = startResizeEvent;
+      document.onmouseup = endResizeEvent;
+      document.onmousemove = handleResizeEvent
 
       tableResizeOverlay.appendChild(handle);
     }
@@ -116,6 +133,9 @@ class DataTable extends Component {
 
   componentDidMount() {
     this.enableResizableColumns(this.refs.table);
+    window['jQuery']('[data-toggle="tooltip"]').tooltip({
+      container: 'body'
+    })
   }
 
   render() {
@@ -135,46 +155,54 @@ class DataTable extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.props.data.map((row, rowIndex) =>
-                (
-                  !this.state.collapsed ||
-                  (this.state.collapsed && rowIndex < this.props.limit) 
-                ) &&
+            {
+              this.props.data
+              .filter((row, rowIndex) => 
+                !this.state.collapsed || 
+                (this.state.collapsed && rowIndex < this.props.limit))
+              .map((row, rowIndex) =>
                 <tr key={rowIndex}>
-                  {this.props.columns.map((column, columnIndex) => 
-                    <td key={columnIndex}>
-                      <span>
-                      {
-                        (
-                          row[column.link]
-                          ? <a href={row[column.link]} target='_blank'>
-                              { row[column.value] }
-                            </a>
-                          : row[column.value]
-                        ) || ''
-                      }
-                      </span>
-                    </td>
-                  )}
-                </tr>)}
+                {
+                  this.props.columns.map((column, columnIndex) => 
+                  <td key={columnIndex}>
+                    <span>
+                    {
+                      (
+                        row[column.link]
+                        ? <a href={row[column.link]} target='_blank'>
+                            { row[column.value] }
+                          </a>
+                        : row[column.value]
+                      ) || ''
+                    }
+                    </span>
+                  </td>
+                  )
+                }
+                </tr>
+              )
+            }
             </tbody>
           </table>
-          { window.jQuery && window.jQuery.fn.tooltip &&  window.jQuery('[data-toggle="tooltip"]').tooltip({container: 'body'}) }
         </div>
         
-        {this.props.data.length > this.props.limit &&
-        <div className='form-group'>
-          <button className='btn btn-default btn-sm'
-            onClick={e => this.setState({collapsed: !this.state.collapsed})}>
-            {this.state.collapsed
-            ? 'Show All'
-            : 'Show Less'}
-          </button>
-        </div>
+        {
+          this.props.data.length > this.props.limit &&
+          <div className='form-group'>
+            <button className='btn btn-default btn-sm'
+              onClick={ e => {
+                  this.setState({collapsed: !this.state.collapsed});
+                  window.setTimeout(f => this.enableResizableColumns(this.refs.table), 0);
+              }}>
+              { this.state.collapsed
+              ? 'Show All'
+              : 'Show Less' }
+            </button>
+          </div>
         }  
       </div>
     );
-  }    
+  }
 }
 
 export default DataTable;
