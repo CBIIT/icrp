372eae9cd1a48e775c80ce16fa77d0af61833f25
unknown <zhoujim@NCI-01979649-L.nci.nih.gov>
2017-01-30 17:44:43 -0500

    Fixed PHP Memory issue

--- modules/custom/db_export_results/src/Controller/ExportResultsController.php
+++ modules/custom/db_export_results/src/Controller/ExportResultsController.php
@@ -31,8 +31,6 @@ use	PHPExcel_Chart;
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
-
-
 ini_set('memory_limit','2048M');
 
 class ExportResultsController extends ControllerBase {
