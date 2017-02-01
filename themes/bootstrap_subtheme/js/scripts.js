4dcd2e797d2a4c9237a53aff781b874febf5b7a9
park-brian <re.public@outlook.com>
2017-02-01 11:27:11 -0500

    added babel-polyfill to globals

--- themes/bootstrap_subtheme/js/scripts.js
+++ themes/bootstrap_subtheme/js/scripts.js
@@ -7,19 +7,10 @@
   }
 })(window.jQuery);
 
-window['enableResizableTableColumns'] = function() {
-  window.setTimeout(function() {
-    $('table:visible').resizableColumns();
-  }, 0)
-}
-
-window['enableTooltips'] = function() {
-  window.setTimeout(function() {
-    $('[data-toggle="tooltip"]').tooltip({container: 'body'}); 
-  }, 0)
-}
-
-$(document).ready(function() {
-  window.enableResizableTableColumns();
-  window.enableTooltips();
-});
+(function ($) {
+  window['enableTooltips'] = function() {
+    window.setTimeout(function() {
+      $('[data-toggle="tooltip"]').tooltip({container: 'body'}); 
+    }, 0);
+  }
+})(window.jQuery);
