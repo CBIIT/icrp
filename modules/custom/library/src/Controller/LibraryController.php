b62b874cf6e6ab40fe122b090114fa26f79d4fe1
Wesley Obenshain <wesley.obenshain@nih.gov>
2017-02-03 17:05:40 -0500

    drupal is stupid

--- modules/custom/library/src/Controller/LibraryController.php
+++ modules/custom/library/src/Controller/LibraryController.php
@@ -3,6 +3,7 @@
 namespace Drupal\library\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LibraryController extends ControllerBase {
   public function content() {
@@ -14,5 +15,48 @@ class LibraryController extends ControllerBase {
         ],
       ],
     ];
+  }
+
+  public function get() {
+    return new JsonResponse(array('show' => 'me this!'));
+  }
+
+  /**
+   * Returns a PDO connection to a database
+   * @param $cfg - An associative array containing connection parameters 
+   *   driver:    DB Driver
+   *   server:    Server Name
+   *   database:  Database
+   *   user:      Username
+   *   password:  Password
+   *
+   * @return A PDO connection
+   * @throws PDOException
+   */
+  function get_connection() {
+    $cfg = [];
+    $icrp_database = \Drupal::config('icrp_database');
+    foreach(['driver', 'host', 'port', 'database', 'username', 'password'] as $key) {
+       $cfg[$key] = $icrp_database->get($key);
+    }
+    // connection string
+    $cfg['dsn'] =
+      $cfg['driver'] .
+      ":Server={$cfg['host']},{$cfg['port']}" .
+      ";Database={$cfg['database']}";
+    // default configuration options
+    $cfg['options'] = [
+      PDO::SQLSRV_ATTR_ENCODING    => PDO::SQLSRV_ENCODING_UTF8,
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  //  PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    // create new PDO object
+    return new PDO(
+      $cfg['dsn'],
+      $cfg['username'],
+      $cfg['password'],
+      $cfg['options']
+    );
   }
 }
