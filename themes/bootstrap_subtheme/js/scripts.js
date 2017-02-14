c232bf05fd9fc6e63ad61815d26b67e61f294aae
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-02-14 12:49:12 -0500

    Contact Us selector.  Fixed link on Welcome page

--- themes/bootstrap_subtheme/js/scripts.js
+++ themes/bootstrap_subtheme/js/scripts.js
@@ -2,7 +2,16 @@
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
       //$('h2').css('color', 'red');
-      $("#edit-keys").attr("placeholder", "Search Website");
+      $("#edit-keys").attr("placeholder", "Search Website2");
+      switch(window.location.pathname) {
+        case "/contact-us":
+          //alert("You are on the contact-us page");
+          var typeOfIssue =decodeURIComponent($.urlParam("type-of-issue"));
+          //alert(typeOfIssue);
+          $('#edit-type-of-issue').val(typeOfIssue);
+          break;
+
+      }
     }
   }
 })(window.jQuery);
@@ -13,4 +22,9 @@
       $('[data-toggle="tooltip"]').tooltip({container: 'body'}); 
     }, 0);
   }
+  $.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+  }
+
 })(window.jQuery);
