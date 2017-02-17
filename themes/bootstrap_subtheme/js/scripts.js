54578b516d42b9243f9743c81e815371b0790237
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-02-17 17:11:46 -0500

    Fixed a script

--- themes/bootstrap_subtheme/js/scripts.js
+++ themes/bootstrap_subtheme/js/scripts.js
@@ -5,23 +5,20 @@
       $("#edit-keys").attr("placeholder", "Search Website");
       switch(window.location.pathname) {
         case "/contact-us":
+          console.log("You are on the /contact-us page.");
           var typeOfIssue =decodeURIComponent($.urlParam("type-of-issue"));
-          $('#edit-type-of-issue').val(typeOfIssue);
+          if(typeOfIssue.length > 0 ) {
+            $('#edit-type-of-issue').val(typeOfIssue);
+          }
             //Strip anchor from organization.
           var currentOrg = $('#edit-organization').val();
-            //alert(current_org);
-            //alert($(current_org).text());
-          var currentOrgStr = $(currentOrg).text();
-          if(typeof currentOrgStr ==='string') {
-            $('#edit-organization').text(currentOrgStr);
-            $('#edit-organization').val(currentOrgStr);
+          console.log('currentOrg: '+currentOrg);
+          console.log('typeof: '+ typeof currentOrg);
+          if(typeof currentOrg ==='string') {
+            console.log($(currentOrg).text());
+            var rawOrg = $(currentOrg).text();
+            $('#edit-organization').val(rawOrg);
           }
-
-          //alert(typeof(currentOrgStr));
-          console.log(currentOrgStr);
-          //$('#edit-organization').val(currentOrgStr);
-
-
           break;
         case "/become-a-partner":
           console.log("You are on the /become-a-partner page.");
@@ -41,7 +38,13 @@
   }
   $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results[1] || 0;
+    var return_val;
+    if(results == null) {
+      return_val = "";
+    } else {
+      return_val = results[1];
+    }
+    return return_val;
   }
 
 })(window.jQuery);
