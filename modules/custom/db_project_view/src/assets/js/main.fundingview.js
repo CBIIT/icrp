045254271c9f29c9e21b3daae072810e11ba78cf
park-brian <re.public@outlook.com>
2017-03-06 15:15:30 -0500

    updated db_funding_view

---
+++ modules/custom/db_project_view/src/assets/js/main.fundingview.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+  	pageLanguage: 'en', 
+  	layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+  }, 'google_translate_element');
+}
