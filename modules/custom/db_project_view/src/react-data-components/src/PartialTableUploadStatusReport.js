e57c906211a42b5ae0c33f47e902789b28545d05
park-brian <re.public@outlook.com>
2017-03-07 10:53:27 -0500

    updated spacing on uploadStatusReport

--- modules/custom/db_project_view/src/react-data-components/src/PartialTableUploadStatusReport.js
+++ modules/custom/db_project_view/src/react-data-components/src/PartialTableUploadStatusReport.js
@@ -32,41 +32,38 @@ export default class PartialTableUploadStatusReport extends Component {
     } = this.props.data;
 
     return (
-      <div className="container_funding_org">
-        <font color="orange"><h1 class="titleOrg margin-right">ICRP Data Upload Status Report</h1></font>
-        <p className="form-group">
-          Information about the status of data submissions and uploads to the ICRP database is included below. Please note that each organization has its own data upload schedule and the latest data uploaded for each organization can be seen <a href="https://icrpartnership-test.org/FundingOrgs">here</a> 
+      <div className="form-group">
+        <h1>ICRP Data Upload Status Report</h1>
+        <p>
+          Information about the status of data submissions and uploads to the ICRP database is included below. Please note that each organization has its own data upload schedule and the latest data uploaded for each organization can be seen 
+          <a href="https://icrpartnership-test.org/FundingOrgs" target="_blank">here</a> 
  	      </p>
-        <div className="row">
-          <div className="col-xs-12">
-            <div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-6 top-buffer">
-            <div>
-              <button onClick={this.exportResult} className="btn btn-default"><span className="glyphicon glyphicon-download-alt"></span>&nbsp;Export</button>
-            </div>
-	  </div>
-          <div className="col-xs-6">
+
+        <div style={{position: 'relative'}}>
+            <button onClick={this.exportResult} className="btn btn-default">
+              <span className="glyphicon glyphicon-download-alt" style={{display: 'inline-block', marginRight: '5px'}}></span>
+              Export
+            </button>
             <Pagination
-              className="pagination pull-right"
+              className="pagination"
+              style={{margin: 0, position: 'absolute', right: 0, bottom: 0}}
               currentPage={pageNumber}
               totalPages={totalPages}
               onChangePage={onPageNumberChange}
             />
-          </div>
         </div>
-        <Table
-          className="table table-bordered table-striped table-condensed table-hover table-narrow table-nowrap"
-          dataArray={page}
-          columns={columns}
-          keys={keys}
-          buildRowOptions={buildRowOptions}
-          sortBy={sortBy}
-          onSort={onSort}
-        />
+
+        <div className="table-responsive">
+          <Table
+            className="table table-bordered table-striped table-condensed table-hover table-narrow table-nowrap"
+            dataArray={page}
+            columns={columns}
+            keys={keys}
+            buildRowOptions={buildRowOptions}
+            sortBy={sortBy}
+            onSort={onSort}
+          />
+        </div>
       </div>
     );
   }
