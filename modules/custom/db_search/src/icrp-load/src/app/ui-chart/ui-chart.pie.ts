8c5169f96458a1ae27061f9bf38ba01e70c1d929
park-brian <re.public@outlook.com>
2017-03-20 11:08:14 -0400

    SS-411: display no-data chart

--- modules/custom/db_search/src/icrp-load/src/app/ui-chart/ui-chart.pie.ts
+++ modules/custom/db_search/src/icrp-load/src/app/ui-chart/ui-chart.pie.ts
@@ -13,12 +13,21 @@ export class PieChart {
         let size = 400;
         let radius = size / 2;
 
+
         let arc: any = d3.arc().outerRadius(radius).innerRadius(radius/2);
         let pie = d3.pie();
 
         let color = d3.scaleOrdinal(d3.schemeCategory20c);
 
         let sum = data.map(e => +e.value).reduce((a, b) => a + b, 0);
+        let empty = data.length === 0;
+
+        if (empty) {
+            data = [{
+                value: 1,
+                label: ''
+            }]
+        }
 
         let svg = host
             .attr('width', '100%')
@@ -36,21 +45,23 @@ export class PieChart {
                 let value = data[index].value;
                 let count = data[index].count;
 
-
                 let html = `
                 <b>${label}</b>
                 <hr style="margin: 2px"/>
                 `;
 
-                if (isNaN(count)) {
-                 html += `Projects: ${Number(value).toLocaleString()} (${(100 * value/sum).toFixed(2)}%)`;
+                if (empty) {
+                    html = 'No Data Available';
+                }
+
+                else if (isNaN(count)) {
+                    html += `Projects: ${Number(value).toLocaleString()} (${(100 * value/sum).toFixed(2)}%)`;
                 }
 
                 else {
-                 html += `Relevance: ${(+Number(value).toFixed(2)).toLocaleString()} (${(100 * value/sum).toFixed(2)}%)`;
-
+                    html += `Relevance: ${(+Number(value).toFixed(2)).toLocaleString()} (${(100 * value/sum).toFixed(2)}%)`;
                     html += `<br>`
-                 html += `Projects: ${Number(count).toLocaleString()}`;
+                    html += `Projects: ${Number(count).toLocaleString()}`;
                 }
 
 
@@ -74,10 +85,8 @@ export class PieChart {
             })
             .each(e => e)
             .attr('d', arc)
-            .style('fill', d => color(d.index.toString()))
-            
-
-
+            .style('fill', d => 
+                empty ? '#ddd' : color(d.index.toString()))
     }
 
     /**
