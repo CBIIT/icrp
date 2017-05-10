947592aa837deb45ab6cccec605f8848caeaf93f
Wesley Obenshain <wesley.obenshain@nih.gov>
2017-05-10 14:28:40 -0400

    custom analytics hit

--- modules/custom/library/src/assets/js/index.js
+++ modules/custom/library/src/assets/js/index.js
@@ -588,5 +588,11 @@ jQuery(function() {
         }
         tree.refresh();
     }
+    $('#library-display').on('click','.item-wrapper a',function(e) {
+        if (typeof ga === "undefined") return;
+        ga('send',{
+            hitType: 'pageview',
+            page: e.target.href
+        });
+    });
 });
-
