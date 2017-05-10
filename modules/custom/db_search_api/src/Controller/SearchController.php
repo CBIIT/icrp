5e4faef13d8542d14630d7db0c9b04b1e69900de
park-brian <re.public@outlook.com>
2017-05-10 13:42:30 -0400

    set default year

--- modules/custom/db_search_api/src/Controller/SearchController.php
+++ modules/custom/db_search_api/src/Controller/SearchController.php
@@ -200,6 +200,13 @@ class SearchController extends ControllerBase {
     $connection = self::getConnection('icrp_load_database');
     $parameters = $request->query->all();
 
+    if ($parameters['type'] === 'project_funding_amounts_by_year' 
+        && !array_key_exists('year', $parameters)) {
+      
+      $year = DatabaseReview::reviewFields($connection)['conversion_years'][0]['value'];
+      $parameters['years'] = DatabaseReview::reviewFields($connection);
+    }
+
     $data = in_array($parameters['type'], self::PARTNER_ANALYTICS_TYPES)
       ? DatabaseSearch::getAnalytics($connection, $parameters)
       : [];
