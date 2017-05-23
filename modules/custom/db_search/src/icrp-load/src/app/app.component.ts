79ffe44c28e4ca59e7f754067c0b4aa1afaeb081
park-brian <re.public@outlook.com>
2017-05-23 12:35:47 -0400

    simplify template

--- modules/custom/db_search/src/icrp-load/src/app/app.component.ts
+++ modules/custom/db_search/src/icrp-load/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'icrp-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div [ngSwitch]="sharedService.get('componentType')">
+      <icrp-review-page *ngSwitchCase="'review'"></icrp-review-page>
+      <icrp-search-page *ngSwitchCase="'search'"></icrp-search-page>
+    </div>
+  `
 })
 export class AppComponent {
-  constructor(private elementRef: ElementRef, public sharedService: SharedService) {
+  constructor(
+    private elementRef: ElementRef,
+    public sharedService: SharedService
+  ) {
     let el = elementRef.nativeElement;
     sharedService.set('apiRoot', el.getAttribute('data-api-root') || '');
     sharedService.set('componentType', el.getAttribute('data-component-type'));
