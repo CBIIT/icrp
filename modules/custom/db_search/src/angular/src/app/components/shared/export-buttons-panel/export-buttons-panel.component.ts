22f6279ad63b5bee8b8ff6ec8e5ea183e7c619cb
Brian Park <re.public@outlook.com>
2017-05-26 08:21:53 -0400

    correct routes used

--- modules/custom/db_search/src/angular/src/app/components/shared/export-buttons-panel/export-buttons-panel.component.ts
+++ modules/custom/db_search/src/angular/src/app/components/shared/export-buttons-panel/export-buttons-panel.component.ts
@@ -9,5 +9,54 @@ import { SharedService } from '../../../services/shared.service';
 export class ExportButtonsPanelComponent  {
   @Input() authenticated: boolean = false;
 
-  constructor(public sharedService: SharedService) { }
+  routes: any = {}
+  /**
+   * 
+   * 
+
+  <icrp-export-button
+    *ngIf="authenticated"
+    label="Export Single"
+    endpoint="/load/ExportResultsSignlePartner">
+  </icrp-export-button>
+
+  <icrp-export-button
+    *ngIf="authenticated"
+    label="Export Abstracts"
+    endpoint="/load/ExportResultsWithAbstractPartner">
+  </icrp-export-button>
+
+  <icrp-export-button
+    *ngIf="authenticated"
+    label="Export Abstracts Single"
+    endpoint="/load/ExportAbstractSignlePartner">
+  </icrp-export-button>
+
+  <span class="pull-right">
+    <icrp-export-button
+      label="Export Graphs"
+      endpoint="/load/ExportResultsWithGraphsPartner">
+    </icrp-export-button>
+   * 
+   * @param sharedService 
+   */
+
+  constructor(public sharedService: SharedService) {
+    let componentType = this.sharedService.get('componentType');
+
+    let baseRoute = {
+      review: '/load',
+      search: ''
+    }[componentType];
+
+    this.routes = {
+      exportResults: this.authenticated
+        ? `${baseRoute}/ExportResultsPartner`
+        : `${baseRoute}/ExportResults`,
+      exportSingle: `${baseRoute}/ExportResultsSignlePartner`,
+      exportAbstracts: `${baseRoute}/ExportResultsWithAbstractPartner`,
+      exportAbstractsSingle: `${baseRoute}/ExportAbstractSignlePartner`,
+      exportGraphs: `${baseRoute}/ExportResultsWithGraphsPartner`,
+    }
+  }
 }
