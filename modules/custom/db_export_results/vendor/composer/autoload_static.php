fef3a9a7727a2cecb9c60e4147baa888f44c7edb
Brian Park <re.public@outlook.com>
2017-06-04 23:26:07 -0400

    add phpexcel using composer

--- modules/custom/db_export_results/vendor/composer/autoload_static.php
+++ modules/custom/db_export_results/vendor/composer/autoload_static.php
@@ -28,11 +28,22 @@ class ComposerStaticInit1431985e958125ad551089251541ebb5
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit1431985e958125ad551089251541ebb5::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit1431985e958125ad551089251541ebb5::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1431985e958125ad551089251541ebb5::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
