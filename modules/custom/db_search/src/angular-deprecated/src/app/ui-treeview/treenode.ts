447a5002c9083234ede1ec88f9e528a0b8d139e9
park-brian <re.public@outlook.com>
2017-06-06 10:30:30 -0400

    remove deprecated files

--- modules/custom/db_search/src/angular-deprecated/src/app/ui-treeview/treenode.ts
+++
@@ -1,9 +0,0 @@
-export interface TreeNode {
-    value: string | number,
-    label: string,
-    children?: TreeNode[],
-    selected?: boolean,
-    showChildren?: boolean,
-    parent?: TreeNode,
-    el?: HTMLElement
-}
