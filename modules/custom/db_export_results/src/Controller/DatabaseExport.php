93f357fe31b94df3506c27991415993e5c8d077a
park-brian <re.public@outlook.com>
2017-07-03 17:10:53 -0400

    add additional columns in search export

--- modules/custom/db_export_results/src/Controller/DatabaseExport.php
+++ modules/custom/db_export_results/src/Controller/DatabaseExport.php
@@ -61,8 +61,10 @@ class DatabaseExport {
       'Projects by CSO' => [
         'query' => 'EXECUTE GetProjectCSOsBySearchID            @SearchID=:search_id',
         'columns' => [
-          'ProjectID'     => 'ICRP Project ID',
-          'CSOCode'       => 'CSO Code',
+          'ProjectID'             => 'ICRP Project ID',
+          'ICRPProjectFundindID'  => 'ICRP Project Funding ID',
+          'AltAwardCode'          => 'Alt. Award Code',
+          'CSOCode'               => 'CSO Code',
         ],
       ],
 
@@ -70,8 +72,11 @@ class DatabaseExport {
       'Projects by Cancer Type' => [
         'query' => 'EXECUTE GetProjectCancerTypesBySearchID     @SearchID=:search_id',
         'columns' => [
-          'ProjectID'     => 'ICRP Project ID',
-          'CancerType'    => 'Cancer Type',
+          'ProjectID'             => 'ICRP Project ID',
+          'ICRPProjectFundingID'  => 'ICRP Project Funding ID',
+          'AltAwardCode'          => 'Alt. Award Code',
+          'ICRPCode'              => 'ICRP Code',
+          'CancerType'            => 'Cancer Type',
         ],
       ],
     ],
@@ -89,9 +94,11 @@ class DatabaseExport {
       'Projects by CSO' => [
         'query' => 'EXECUTE GetProjectCSOsBySearchID            @SearchID=:search_id',
         'columns' => [
-          'ProjectID'     => 'ICRP Project ID',
-          'CSOCode'       => 'CSO Code',
-          'CSORelevance'  => 'Relevance',
+          'ProjectID'             => 'ICRP Project ID',
+          'ICRPProjectFundindID'  => 'ICRP Project Funding ID',
+          'AltAwardCode'          => 'Alt. Award Code',
+          'CSOCode'               => 'CSO Code',
+          'CSORelevance'          => 'Relevance',
         ],
       ],
 
@@ -99,9 +106,12 @@ class DatabaseExport {
       'Projects by Cancer Type' => [
         'query' => 'EXECUTE GetProjectCancerTypesBySearchID     @SearchID=:search_id',
         'columns' => [
-          'ProjectID'     => 'ICRP Project ID',
-          'CancerType'    => 'Cancer Type',
-          'Relevance'     => 'Relevance',
+          'ProjectID'             => 'ICRP Project ID',
+          'ICRPProjectFundingID'  => 'ICRP Project Funding ID',
+          'AltAwardCode'          => 'Alt. Award Code',
+          'ICRPCode'              => 'ICRP Code',
+          'CancerType'            => 'Cancer Type',
+          'Relevance'             => 'Relevance',
         ],
       ],
     ],
