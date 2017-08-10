25d8de150a3fe07fc1c99d11aa537a6b154e65c8
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-08-10 15:57:14 -0400

    Adding upgrade_production_8.3.6

---
+++ utility/upgrade_production_8.3.6/create_icrp_8.2.7.sh
@@ -0,0 +1,115 @@
+#
+# Creates a Drupal 8.2.7 Website from Scratch.
+#
+
+if [ "$5" = "" ]
+then
+  echo "**"
+  echo "* Syntax: $0 <DOC_ROOT> <DB_NAME> <DB_USER> <DB_PASSWORD> <SETTINGS_PHP_PATH>"
+  echo "* Enter the document root, db name, db user, and db password"
+  echo "* Enter the FULL path to the settings.php file to replace after the build"
+  echo "**"
+  exit
+fi
+DOC_ROOT=$1 
+DB_NAME=$2
+DB_USER=$3
+DB_PASS=$4
+SETTINGS_PHP_PATH=$5
+# Clear cache - causing problems.  Needed sudo to remove root files.
+echo "**"
+echo "* Clear cache files causing problems."
+echo "**"
+ls -latr ~/.composer/cache/files/doctrine/orm
+ls -latr ~/.composer/cache/files//incenteev/composer-parameter-handler
+ls -latr ~/.composer/cache/files//jdorn/sql-formatter
+sudo rm -f ~/.composer/cache/files//doctrine/orm/*
+sudo rm -f ~/.composer/cache/files//incenteev/composer-parameter-handler/*
+sudo rm -f ~/.composer/cache/files//jdorn/sql-formatter/*
+sudo rm -rf ~/.composer/cache/files/*
+
+#rm all files
+echo "**"
+echo "* Remove all files from $DOC_ROOT"
+echo "**"
+echo "mkdir -p $DOC_ROOT"
+rm -rf $DOC_ROOT
+mkdir -p $DOC_ROOT
+
+echo "**"
+echo "* Composer init"
+echo "**"
+composer create-project drupal-composer/drupal-project:8.x-dev $DOC_ROOT --stability dev --no-interaction
+
+cd $DOC_ROOT
+#Download shell
+echo "**"
+echo "* Create 8.2.7 site"
+echo "**"
+git clone https://github.com/genesis55/myproject.git
+cd myproject
+git checkout tags/d8.2.7
+
+cd ..
+cp myproject/composer.json .
+rm -rf myproject
+echo "You should have your composer.json"
+
+# Run composer update
+echo "**"
+echo "* composer update drupal/core --with-dependencies\n**"
+echo "**"
+composer update drupal/core --with-dependencies
+
+
+echo "**"
+echo "* The next command may need your github username and password"
+echo "* git clone https://<username>:<password>@github.com/CBIIT/icrp.git\n**"
+echo "**"
+cd sites
+git clone https://github.com/CBIIT/icrp.git
+#git clone https://<username>:<password>@github.com/CBIIT/icrp.git
+
+cd icrp
+echo "**"
+echo "* Switching to 1.2"
+echo "**"
+git checkout tags/icrp_1.2.0_20170718
+
+#cp modules, themes, libraries, and default directories
+echo "**"
+echo "* Copy themes and libraries"
+echo "**"
+cp -r modules/* ../../modules
+cp -r themes/* ../../themes
+mkdir -p ../../libraries
+cp -r libraries/* ../../libraries
+cp -r sites/default/ ../default/
+
+#cp settings.php to the new site.
+echo "**"
+echo "* Copy themes and libraries"
+echo "**"
+echo "cp $SETTINGS_PHP_PATH/settings.php ../default/"
+cp $SETTINGS_PHP_PATH/settings.php ../default/
+
+#import database to icrp
+echo "**"
+echo "* Import database"
+echo "**"
+mysql -u$DB_USER -p$DB_PASS $DB_NAME < icrp.sql
+drush cr
+echo "**"
+echo "* ICRP 1.2 is ready"
+echo "**"
+echo "**"
+echo "* Install Production database now and clear cache."
+echo "**"
+echo "**"
+echo "* STEP 2:"
+echo "* NEXT: Run upgrade_icrp_8.3.6.sh"
+echo "**"
+
+
+
+
