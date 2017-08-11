cb95125f6430d3b8c45163ca63ba8d72b2595423
Wesley Obenshain <umdshaman@gmail.com>
2017-08-11 02:12:22 -0400

    Updated Views

--- database/MYSQL/Schema/Views.sql
+++ database/MYSQL/Schema/Views.sql
@@ -1,44 +1,4 @@
 -- -----------------------------------------------------------------------------------------------------------------------------------
-<<<<<<< Updated upstream
---  VIEW - vwProjects
--- -----------------------------------------------------------------------------------------------------------------------------------
-
-DROP VIEW IF EXISTS vwProjects;
-
-CREATE VIEW vwProjects AS
-SELECT DISTINCT
-  p.`ProjectID`,
-  pf.`ProjectFundingID`,
-  p.`AwardCode`,
-  p.`IsChildhood`,
-  fi.`LastName`  AS piLastName,
-  fi.`FirstName` AS piFirstName,
-  fi.`ORC_ID` AS piORCiD,
-  i.`Name` AS institution,
-  i.`City`,
-  i.`State`,
-  i.`Country`,
-  fo.`FundingOrgID` AS FundingOrgID,
-  fo.`Abbreviation` AS FundingOrg,
-  fo.`Type` AS FundingOrgType,
-  pt.`ProjectType`,
-  ct.`CancerTypeID`,
-  c.`Name` AS CancerType,
-  cso.`CSOCode`,
-  ext.`CalendarYear`,
-  s.`Content` AS ProjectContent
-FROM `Project` p
-  JOIN `ProjectFunding` pf ON p.`ProjectID` = pf.`ProjectID`
-  JOIN `ProjectFundingExt` ext ON pf.`ProjectFundingID` = ext.`ProjectFundingID`
-  JOIN `ProjectFundingInvestigator` fi ON fi.`ProjectFundingID` = pf.`ProjectFundingID`
-  JOIN `Institution` i ON i.`InstitutionID` = fi.`InstitutionID`
-  JOIN `FundingOrg` fo ON fo.`FundingOrgID` = pf.`FundingOrgID`
-  JOIN `Project_ProjectType` pt ON p.`ProjectID` = pt.`ProjectID`
-  JOIN `ProjectCancerType` ct ON pf.`ProjectFundingID` = ct.`ProjectFundingID`
-  JOIN `CancerType` c ON c.`CancerTypeID` = ct.`CancerTypeID`
-  JOIN `ProjectCSO` cso ON pf.`ProjectFundingID` = cso.`ProjectFundingID`
-  JOIN `ProjectSearch` s ON s.`ProjectID` = p.`ProjectID`;
-=======
 --  VIEW - vwProjectCores
 -- -----------------------------------------------------------------------------------------------------------------------------------
 DROP VIEW IF EXISTS vwProjectCores;
@@ -53,7 +13,6 @@ SELECT
 FROM `Project` p
   JOIN `Project_ProjectType` pt ON pt.`ProjectID` = p.`ProjectID`
 GROUP BY p.`ProjectID`;
->>>>>>> Stashed changes
 
 -- -----------------------------------------------------------------------------------------------------------------------------------
 --  VIEW - vwProjectFundings
@@ -81,15 +40,11 @@ SELECT DISTINCT
   pf.`FundingOrgID`,
   o.`Name` AS `FundingOrg`,
   o.`Abbreviation` AS `FundingOrgShort`,
-  o.`Type` AS `FundingOrgType`,
-  GREATEST(p.`UpdatedDate`, pf.`UpdatedDate`, fi.`UpdatedDate`, i.`UpdatedDate`, o.`UpdatedDate`) AS LatestUpdate
+  o.`Type` AS `FundingOrgType`
 FROM `Project` p
   JOIN `ProjectFunding` pf ON p.`ProjectID` = pf.`ProjectID`
   JOIN `ProjectFundingInvestigator` fi ON fi.`ProjectFundingID` = pf.`ProjectFundingID`
   JOIN `Institution` i ON i.`InstitutionID` = fi.`InstitutionID`
-<<<<<<< Updated upstream
-  JOIN `FundingOrg` o ON pf.`FundingOrgID` = o.`FundingOrgID`;
-=======
   JOIN `FundingOrg` o ON o.`FundingOrgID` = pf.`FundingOrgID`;
 
 -- -----------------------------------------------------------------------------------------------------------------------------------
@@ -98,29 +53,21 @@ FROM `Project` p
 DROP VIEW IF EXISTS vwProjectExtensions;
 
 CREATE VIEW vwProjectExtensions AS
-SELECT DISTINCT
-  pf.`ProjectID`,
-  pf.`ProjectFundingID`,
-  pf.`piLastName`,
-  pf.`piFirstName`,
-  pf.`piORCiD`,
-  pf.`Institution`,
-  pf.`City`,
-  pf.`State`,
-  pf.`Country`,
-  pf.`FundingOrgID`,
-  pf.`FundingOrgShort` AS FundingOrg,
-  pf.`FundingOrgType`,
-  ct.`CancerTypeID`,
-  CONVERT(GROUP_CONCAT(CONVERT(c.`Name` USING utf8)) USING ucs2) AS CancerType,
-  cso.`CSOCode`,
-  ext.`CalendarYear`,
-  GREATEST(pf.`LatestUpdate`, ext.`UpdatedDate`, ct.`UpdatedDate`, c.`UpdatedDate`, cso.`UpdatedDate`) AS LatestUpdate
-FROM `vwProjectFundings` pf
-  JOIN `ProjectFundingExt` ext ON ext.`ProjectFundingID` = pf.`ProjectFundingID`
-  JOIN `ProjectCancerType` ct ON ct.`ProjectFundingID` = ext.`ProjectFundingID`
+SELECT
+	`ProjectID`, pf.`ProjectFundingID`, `Title`, `FundingContact`, `BudgetStartDate`, `BudgetEndDate`, `Amount`, pf.`FundingOrgID`,
+	`LastName` AS `piLastName`, `FirstName` AS `piFirstName`, `ORC_ID` AS `piORCiD`,
+	i.`Name` AS `Institution`, `City`, `State`, i.`Country`,
+	o.`Name` AS `FundingOrg`, o.`Abbreviation` AS `FundingOrgShort`, o.`Type` AS `FundingOrgType`,
+	c.`CancerTypeID`, c.`Name` AS `CancerType`,
+	GROUP_CONCAT(DISTINCT ext.CalendarYear) AS CalendarYears,
+	GROUP_CONCAT(DISTINCT cso.CSOCode) AS CSOCodes
+FROM `ProjectFunding` pf
+  JOIN `ProjectFundingInvestigator` fi ON fi.`ProjectFundingID` = pf.`ProjectFundingID`
+  JOIN `Institution` i ON i.`InstitutionID` = fi.`InstitutionID`
+  JOIN `FundingOrg` o ON o.`FundingOrgID` = pf.`FundingOrgID`
+  JOIN `ProjectCancerType` ct ON ct.`ProjectFundingID` = pf.`ProjectFundingID`
   JOIN `CancerType` c ON c.`CancerTypeID` = ct.`CancerTypeID`
-  JOIN `ProjectCSO` cso ON cso.`ProjectFundingID` = ext.`ProjectFundingID`
-GROUP BY pf.`ProjectID`, cso.`CSOCode`, ext.`CalendarYear`
-ORDER BY GREATEST(pf.`LatestUpdate`, ext.`UpdatedDate`, ct.`UpdatedDate`, c.`UpdatedDate`, cso.`UpdatedDate`);
->>>>>>> Stashed changes
+  JOIN `ProjectFundingExt` ext ON ext.`ProjectFundingID` = pf.`ProjectFundingID`
+  JOIN `ProjectCSO` cso ON cso.`ProjectFundingID` = pf.`ProjectFundingID`
+GROUP BY pf.`ProjectFundingID`, c.`CancerTypeID`
+ORDER BY `ProjectID`, pf.`ProjectFundingID`, c.`CancerTypeID`;
