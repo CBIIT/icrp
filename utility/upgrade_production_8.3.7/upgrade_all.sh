d6fc6c84b77ca851fd90b4ef551ebef6c5e84390
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-08-22 15:21:32 -0400

    Clarified script

--- utility/upgrade_production_8.3.7/upgrade_all.sh
+++ utility/upgrade_production_8.3.7/upgrade_all.sh
@@ -14,33 +14,34 @@ rm -rf tmp
 rm -rf icrp
 mkdir tmp icrp
 chown -R drupal:drupal tmp icrp
-chmod -R 775 tmp/utility/upgrade_production_8.3.7
 
 #Change to DRUPAL USER
 sudo su drupal
 git clone https://github.com/CBIIT/icrp.git tmp
+chmod -R 775 tmp/utility/upgrade_production_8.3.7
 
 #STEP 1: Drupal 8.2.7
 tmp/utility/upgrade_production_8.3.7/create_icrp_8.2.7.sh icrp
 
 #STEP 2: Move ICRP assets (As SuperUser)
 sudo su -  (or exit from drupal user)
-chmod 775 -R tmp/utility/upgrade_production_8.3.7
+
 tmp/utility/upgrade_production_8.3.7/move_icrp.sh icrp
 
 #STEP 3: Upgrade to 8.3.7
 sudo chmod -R 777 icrp/sites icrp/themes icrp/modules icrp/libraries icrp/composer.json
-#Change to DRUPAL USER
+#Change to DRUPAL USER (Run as Drupal user)
+sudo su drupal
 tmp/utility/upgrade_production_8.3.7/upgrade_icrp_8.3.7.sh icrp
 
-#STEP 4: Remove layout_plugin
+#STEP 4: Remove layout_plugin (Run as Drupal user)
 tmp/utility/upgrade_production_8.3.7/remove_layout_plugin.sh icrp
-#Change to root
 
 #STEP 5: Upgrade the rest of the modules
-#Run this as DRUPAL USER
+#Run this as DRUPAL USER (Run as Drupal user)
 tmp/utility/upgrade_production_8.3.7/upgrade_modules.sh icrp
 #Change to ROOT
+sudo su - (or exit from drupal user)
 sudo chmod -R 755 icrp/sites icrp/themes icrp/modules icrp/libraries
 sudo chmod -R 744 icrp/composer.json
 #clean up
