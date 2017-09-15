57757737b6a44b23bd875074cc6c970a62d35683
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-09-15 16:57:15 -0400

    Added jqueryvalidate and script to run post-create-project-cmd

--- utility/composer_setup/makeit.sh
+++ utility/composer_setup/makeit.sh
@@ -52,6 +52,7 @@ echo "**"
 echo "* Copy composer.json.8.2.7 and run composer update "
 echo "**"
 cp ../composer.json.8.3.7 ./composer.json
+cp ../missing_argument_4_in_2743715-6.patch .
 
 echo "You should have your composer.json"
 
