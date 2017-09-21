7485dcfd03118f5f041337e9e009ceb2dc0bda1f
park-brian <re.public@outlook.com>
2017-09-21 10:58:51 -0400

    ss-884: add export icon

--- modules/custom/db_map/src/React/src/components/App/App.tsx
+++ modules/custom/db_map/src/React/src/components/App/App.tsx
@@ -164,9 +164,16 @@ export default class App extends React.Component<{}, AppState> {
             className="btn btn-default btn-sm margin-top margin-bottom"
             onClick={event => this.export()}
           >
-            <span className="margin-right">
-              Export
-            </span>
+            <div className="display-flex align-items-center">
+              <svg width="12px" height="12px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <g id="desktop-download" stroke="none" stroke-width="1" fill-rule="evenodd" fill="#000000">
+                  <path d="M4,6 L7,6 L7,0 L9,0 L9,6 L12,6 L8,10 L4,6 L4,6 Z M15,2 L11,2 L11,3 L15,3 L15,11 L1,11 L1,3 L5,3 L5,2 L1,2 C0.45,2 0,2.45 0,3 L0,12 C0,12.55 0.45,13 1,13 L6.34,13 C6.09,13.61 5.48,14.39 4,15 L12,15 C10.52,14.39 9.91,13.61 9.66,13 L15,13 C15.55,13 16,12.55 16,12 L16,3 C16,2.45 15.55,2 15,2 L15,2 Z" id="Shape"></path>
+                </g>
+              </svg>
+              <span className="margin-left">
+                Export
+              </span>
+            </div>
           </button>
         }
 
