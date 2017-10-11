b11e6ed1ddf5fb26a87efa243e8a606e5f30fb1f
park-brian <re.public@outlook.com>
2017-10-11 17:31:28 -0400

    update validators

--- modules/custom/db_admin/src/AddFundingOrganization/src/app/funding-organization-form/funding-organization-form.component.ts
+++ modules/custom/db_admin/src/AddFundingOrganization/src/app/funding-organization-form/funding-organization-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'icrp-funding-organization-form',
@@ -12,22 +12,61 @@ export class FundingOrganizationFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.form = this.createForm();
+
+    this.form.controls['latitude'].valueChanges
+      .subscribe(value => console.log(value))
   }
 
   createForm(): FormGroup {
-    return this.formBuilder.group({
+    let formGroup = this.formBuilder.group({
       partner: ['', Validators.required],
       memberType: ['associate', Validators.required],
-      name: '',
-      abbreviation: '',
-      organizationType: '',
-      latitude: null,
-      longitude: null,
-      country: '',
-      currency: '',
-      note: '',
+      name: ['', Validators.required],
+      abbreviation: ['', Validators.required],
+      organizationType: ['', Validators.required],
+      latitude: [null,],
+      longitude: [null],
+      country: ['', Validators.required],
+      currency: ['', Validators.required],
+      note: ['', Validators.maxLength(1000)],
       annualizedFunding: false,
     });
+
+    let latitude = formGroup.controls.latitude;
+    let longitude = formGroup.controls.longitude;
+
+    latitude.setValidators([
+      Validators.min(-90),
+      Validators.max(90),
+      this.requireWith(longitude)
+    ])
+
+    longitude.setValidators([
+      Validators.min(-90),
+      Validators.max(90),
+      this.requireWith(latitude)
+    ]);
+
+    latitude.valueChanges.subscribe(value =>
+      longitude.updateValueAndValidity({emitEvent: false})
+    );
+
+    longitude.valueChanges.subscribe(value =>
+      latitude.updateValueAndValidity({emitEvent: false})
+    );
+
+    return formGroup;
+  }
+
+  requireWith(otherControl: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      let invalid = (control.value && !otherControl.value)
+        || (otherControl.value && !control.value);
+
+      return invalid
+        ? { requireWith: true }
+        : null;
+    }
   }
 
   ngOnInit() {
