9fd1aaf272294392d0802e13d40acc868e1a3d70
Wesley Obenshain <umdshaman@gmail.com>
2017-11-05 23:05:45 -0500

    SS709

--- modules/custom/db_project_view/src/react-data-components/build.sh
+++ modules/custom/db_project_view/src/react-data-components/build.sh
@@ -1,5 +1,5 @@
 npm run build
 cp build/static/js/main*.js ../assets/js/main.fundingorg.js
 cp build/static/css/main*.css ../assets/css/main.fundingorg.css
-cd /var/www/html
+cd /var/www/html/
 drush cr
