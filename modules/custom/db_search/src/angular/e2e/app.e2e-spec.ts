730b2e01f9468a484e9d6bad03b26dbb039b0f0b
Brian Park <park-brian@users.noreply.github.com>
2017-11-07 17:01:08 -0500

    ss-776: update package.json

--- modules/custom/db_search/src/angular/e2e/app.e2e-spec.ts
+++ modules/custom/db_search/src/angular/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('angular App', () => {
+describe('icrp App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('angular App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to icrp!');
   });
 });
