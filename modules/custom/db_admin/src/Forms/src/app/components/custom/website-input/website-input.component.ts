b061acce8355c26d90038c9a2095d94775df1174
Brian Park <park-brian@users.noreply.github.com>
2017-11-13 17:52:45 -0500

    ss-203: allow query parameters in website url

--- modules/custom/db_admin/src/Forms/src/app/components/custom/website-input/website-input.component.ts
+++ modules/custom/db_admin/src/Forms/src/app/components/custom/website-input/website-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, ElementRef, ViewChild, forwardRef } from '@angular/core';
 import { ControlValueAccessor, FormGroup, FormBuilder, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { defaultIfEmpty, map } from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 import * as parse from 'url-parse';
 
 @Component({
@@ -16,106 +16,201 @@ import * as parse from 'url-parse';
 })
 export class WebsiteInputComponent implements ControlValueAccessor {
 
+  /**
+   * Contains form controls for a WebsiteInputComponent
+   *
+   * @type {FormGroup}
+   * @memberof WebsiteInputComponent
+   */
   form: FormGroup;
 
+  /**
+   * Contains allowed protocol identifiers
+   *
+   * @memberof WebsiteInputComponent
+   */
   protocols = [
     {label: 'http://', value: 'http:'},
     {label: 'https://', value: 'https:'},
   ];
 
+  /**
+   * Placeholder text for the input
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   placeholder: string = 'Enter website'
 
+  /**
+   * Global css classes to be applied to the select element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   selectClass: string;
 
+  /**
+   * CSS styles to be applied to the select element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   selectStyle: string;
 
+  /**
+   * Global css classes to be applied to the input element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   inputClass: string;
 
+  /**
+   * Css styles to be applied to the input element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   inputStyle: string;
 
+  /**
+   * ID attribute to set for the input element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   inputId: string;
 
+  /**
+   * Name attribute to set for the input element
+   *
+   * @type {string}
+   * @memberof WebsiteInputComponent
+   */
   @Input()
   inputName: string;
 
-  @Input('value')
-  _value: string;
+  /**
+   * The current value of this component
+   *
+   * @private
+   * @type {(string | null)}
+   * @memberof WebsiteInputComponent
+   */
+  private _value: string | null = null;
 
-
+  /**
+   * Creates an instance of WebsiteInputComponent.
+   * @param {FormBuilder} formBuilder Angular FormBuilder service
+   * @memberof WebsiteInputComponent
+   */
   constructor(private formBuilder: FormBuilder) {
+
     this.form = formBuilder.group({
       protocol: 'http:',
       resource: '',
     });
 
-    let { protocol, resource } = this.form.controls;
+    const { protocol, resource } = this.form.controls;
 
-    let protocol$ = protocol.valueChanges;
-    let resource$ = resource.valueChanges.pipe(
-      map((value: string) => {
+    // initialize observables for input value changes
+    const protocol$ = protocol.valueChanges;
 
-      let resourceValue = value || '';
-      if (/^https?:\/\//.test(value)) {
-        let url = parse(value);
-        resourceValue = `${url.host}${url.pathname}`;
-        this.form.patchValue({
-          protocol: url.protocol,
-          resource: resourceValue
-        });
-      }
+    // if a website's url was pasted into the input,
+    // remove the protocol from the supplied url,
+    // set the protocol selector to the appropriate value,
+    // and set the input's value to the truncated url
+    const resource$ = resource.valueChanges.pipe(
+      map((value: string = '') => {
+        if (/^https?:\/\//.test(value)) {
+          const url = parse(value);
+          const resourceValue = `${url.host}${url.pathname}`;
 
-      return resourceValue
-    }));
+          // update the form controls to reflect the new value
+          // when valueChanges emits a new value, that value
+          // will be used instead of the current value
+          this.form.patchValue({
+            protocol: url.protocol,
+            resource: resourceValue
+          });
 
+          return resourceValue;
+        } else {
+          return value;
+        }
+      }),
+    );
+
+    // update the current value whenever an input changes
     Observable.combineLatest(
       protocol$,
       resource$,
-      this.parseInput
+      (protocol, resource) => `${protocol}//${resource}`
     ).subscribe(value => this.value = value)
 
     protocol.updateValueAndValidity();
     resource.updateValueAndValidity();
   }
 
-  parseInput(protocol, resource) {
-    return `${protocol}//${resource}`
-  }
-
+  /**
+   * Gets the value property
+   *
+   * @memberof WebsiteInputComponent
+   */
   get value() {
     return this._value;
   }
 
-  set value(value) {
-    let url = parse(value);
-    let path = `${url.host}${url.pathname}`;
+  /**
+   * Sets the value property
+   *
+   * @param {string} value The new value to assign to this component
+   * @memberof WebsiteInputComponent
+   */
+  set value(value: string) {
 
-    this._value = path
-      ? `${url.protocol}//${path}`
-      : null;
+    if (value) {
+      let url = parse(value);
+      console.log(url);
+      let protocol = url.protocol || 'http:';
+      let resource = `${url.host}${url.pathname}${url.query}`;
 
-    this.form.patchValue({
-      protocol: url.protocol,
-      resource: `${url.host}${url.pathname}`
-    }, {emitEvent: false});
+      this._value = resource
+        ? url.href : null;
+
+      this.form.patchValue(
+        {protocol, resource},
+        {emitEvent: false}
+      );
+    } else {
+      this._value = null;
+    }
+
     this.onChange(this._value);
   }
 
-  onChange(value: string) {}
+  onChange(value: string) {
+    this.form.controls.protocol.updateValueAndValidity({emitEvent: false});
+    this.form.controls.resource.updateValueAndValidity({emitEvent: false});
+  }
+
   registerOnTouched() {}
   registerOnChange(fn) {
     this.onChange = fn;
   }
 
+  // writes the current value to value property
   writeValue(value: string) {
-    this.form.reset({
+    this.form.patchValue({
       protocol: 'http:',
-      resource: value
+      resource: value || ''
     });
   }
 }
