4c9e79484b4656023746cb301fcd1346ab1e037d
Wesley Obenshain <wesley.obenshain@nih.gov>
2017-12-08 16:20:05 -0500

    rewrote to not update nodes every time

--- modules/custom/icrp/src/Importer/OrganizationRestClient.php
+++ modules/custom/icrp/src/Importer/OrganizationRestClient.php
@@ -61,69 +61,29 @@ class OrganizationRestClient {
      * @params array $organizations
     */
     private static function checkOrganizations(array $organizations) {
-        // Get a list of current organizations
-        $sql = "SELECT field_organization_id_value FROM node__field_organization_id;";
-        $current_organization_ids = db_query($sql)->fetchCol();
-        //drupal_set_message(print_r($current_organization_ids, true));
-
-        // If Organization doesn't exist then save into database
-
-        foreach ($organizations as $key => $organization) {
-            $organization_id = $organization["ID"];
-            //drupal_set_message($organization['ID'].", ".$organization['Name'].", ".$organization['IsActive']);
-            //Look up Organization $nid
-            \Drupal::logger('icrp')->notice("Looking for: ".$organization['ID']);
-
-            if (!in_array(intval($organization['ID']), $current_organization_ids)) {
-                //drupal_set_message("Adding: org: ".$organization['ID']);
-                self::addOrganization($organization);
+        $sql = "SELECT field_organization_id_value, entity_id FROM node__field_organization_id;";
+        $current_organization_ids = db_query($sql)->fetchAllAssoc('field_organization_id_value',PDO::FETCH_ASSOC);
+        foreach ($organizations as $key=>$organization) {
+            if (array_key_exists($organization['ID'],$current_organization_ids)) {
+                $node = Node::load($current_organization_ids[$organization['ID']]['entity_id']);
+                $nodeArray = $node->toArray();
+                if (($nodeArray['title'][0]['value'] != $organization['Name']) || ($nodeArray['body'][0]['value'] != $organization['Name']) || ($nodeArray['status'][0]['value'] != $organization['IsActive'])) {
+                    $node->set('title', $organization['Name']);
+                    $node->set('body', $organization['Name']);
+                    $node->set('status', intval($organization['IsActive']));
+                    $node->save();
+                }
             } else {
-                //drupal_set_message("Update org: ".$organization['ID']);
-                self::updateOrganization($organization);
+                self::addOrganization($organization);
             }
-
         }
     }
 
-    /*
-    * Update an existing organization to organization entity.
-    * @param array $organization
-    */
-    private static function updateOrganization(array $organization) {
-        //Lookup node by organization ID
-        $query = \Drupal::entityQuery('node')
-            ->condition('type', 'organization', '=')
-            ->condition('field_organization_id', $organization['ID'], '=');
-        $nids = $query->execute();
-        //drupal_set_message("nids = ".print_r($nids, true));
-        $nid = 0;
-        foreach($nids as $key => $value) {
-            //drupal_set_message($key." => ".$value);
-            $nid = $value;
-        }
-        if($nid != 0) {
-            //\Drupal::logger('icrp')->notice("Updataing Organization: ID: " . $organization['ID']);
-            //drupal_set_message("Updataing Organization: ID: " . $organization['ID'], 'notice');
-            //drupal_set_message("Organization Details: " .print_r($organization, true), 'notice');
-            $node = Node::load($nid);
-            $node->set('title', $organization['Name']);
-            $node->set('body', $organization['Name']);
-            $node->set('status', intval($organization['IsActive']));
-            $node->save();
-            //drupal_set_message("SAVED A NODE:");
-
-        } else {
-            \Drupal::logger('icrp')->warning("Organization Not found: ID:" . $organization['ID']);
-            //drupal_set_message("Organization not found nid: ".$nid, 'warning');
-        }
-
-    }
     /*
     * Add new organization to organization entity.
     * @param array $organization
     */
     private static function addOrganization(array $organization) {
-
         \Drupal::logger('icrp')->notice("Adding " . $organization['Name'] . " to organization.");
         $node = Node::create(array('title' => $organization['Name'],
                                     'body' => $organization['Name'],
@@ -145,9 +105,9 @@ class OrganizationRestClient {
    * @return A PDO connection
    * @throws PDOException
    */
-  private static function get_connection() {
+  private static function get_connection($database_name='icrp_database') {
     $cfg = [];
-    $icrp_database = \Drupal::config('icrp_database');
+    $icrp_database = \Drupal::config($database_name);
     foreach(['driver', 'host', 'port', 'database', 'username', 'password'] as $key) {
        $cfg[$key] = $icrp_database->get($key);
     }
