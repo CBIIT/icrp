f539140bb06ade9a197f2d621b5ffcc847601d68
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2017-12-20 10:37:44 -0500

    Added fixes to About Us and export icrp forum

--- sites/default/sync/content/forum/export_icrp_forums.sh
+++ sites/default/sync/content/forum/export_icrp_forums.sh
@@ -15,7 +15,7 @@ drush sset system.maintenance_mode 1
 echo "* Exporting Drupal 8 User and Webform tables..."
 
 # Export all forum related tables -  node, comment, taxonomy, history...etc  
-mysqldump $mysql_database -u$user -p$password node node__body node__comment node__comment_forum node__taxonomy_forums node_field_data node_field_revision node_revision node_revision__body node_revision__comment_forum node_revision__taxonomy_forums taxonomy_index tracker_node tracker_user user__field_last_forum_visit comment comment__comment_body comment_entity_statistics comment_field_data history forum forum_index > $target_file
+mysqldump $mysql_database -u$user -p$password node node_access node__body node__comment node__comment_forum node__taxonomy_forums node_field_data node_field_revision node_revision node_revision__body node_revision__comment_forum node_revision__taxonomy_forums taxonomy_index tracker_node tracker_user user__field_last_forum_visit comment comment__comment_body comment_entity_statistics comment_field_data history forum forum_index > $target_file
 
 echo "* Export completed. $target_file "
 
