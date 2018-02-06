246fc07f3374f91dde6bf5645488dd9c23a75505
Brian Park <park-brian@users.noreply.github.com>
2018-02-06 16:52:23 -0500

    ss-1163: remove call to parent constructor

--- modules/custom/library/src/Controller/LibraryController.php
+++ modules/custom/library/src/Controller/LibraryController.php
@@ -15,8 +15,6 @@ use Zipstream\ZipStream;
 class LibraryController extends ControllerBase {
 
   function __construct() {
-    parent::__construct();
-
     // ensure the uploads folder exists
     $uploads_folder = \Drupal::config('library')->get('uploads_folder') ?? 'data/library/uploads';
     if (!file_exists($uploads_folder)) {
