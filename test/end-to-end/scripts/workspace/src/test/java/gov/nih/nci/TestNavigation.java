a10e92007feb7551af8729a1443c34e54e4f9c2f
trantt <tin.tran@nih.gov>
2018-02-13 15:28:12 -0500

    Update to Test Navigation script.
    Update to Test Navigation script.

--- test/end-to-end/scripts/workspace/src/test/java/gov/nih/nci/TestNavigation.java
+++ test/end-to-end/scripts/workspace/src/test/java/gov/nih/nci/TestNavigation.java
@@ -401,13 +401,13 @@ public class TestNavigation {
 				Test.clickLink(By.linkText("Twitter"));
 				Test.wait_For(5000);
 				Test.wait_until_element_present(By.name("We've updated our Privacy Policy, effective June 18th, 2017. You can learn more about what's changed on our Help Center. "),"We've updated our Privacy Policy, effective June 18th, 2017. You can learn more about what's changed on our Help Center. ");
-				//Test.switchToChildBrowser();
+				Test.switchToChildBrowser();
 				//Test.GoToNewTabWindow();
 				Test.wait_For(3000);
-				Test.verifyUrl("https://twitter.com/icrpartners1"); //https://twitter.com/icrpartners1?ref_src=twsrc%5Etfw
-				//Test.verifyUrl("https://twitter.com/icrpartners1?ref_src=twsrc%5Etfw");
-				Test.wait_For(1000);
-				//Test.switchToParentBrowser();
+				//Test.verifyUrl("https://twitter.com/icrpartners1"); //https://twitter.com/icrpartners1?ref_src=twsrc%5Etfw
+				/Test.verifyUrl("https://twitter.com/icrpartners1?ref_src=twsrc%5Etfw");
+				Test.wait_For(2000);
+				Test.switchToParentBrowser();
 				//Test.GoToPreviousTabWindow();
 				
 				//Verify Library Page
