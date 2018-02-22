9b7a8a1bc69849b01440b93de3bbd0375940bad0
Brian Park <park-brian@users.noreply.github.com>
2018-02-22 13:23:08 -0500

    update partners table

--- modules/custom/icrp_partners/src/Assets/js/main.js
+++ modules/custom/icrp_partners/src/Assets/js/main.js
@@ -5,10 +5,14 @@ $(function() {
     $('.nav-tabs a[href="#partners"]').on('shown.bs.tab', function(event) {
         if (!initialized) {
             table = $('#partners table').DataTable({
-                paging: false,
-                ordering: false,
-                dom: "<'table-responsive't>",
+                pagingType: 'simple_numbers',
+                lengthMenu: [ 5, 10, 25 ],
+                dom: "<'d-flex-responsive'<'mr-40 mv-5'f><'mr-40 mv-5'L><'mv-5'p>>"
+                   + "<'table-responsive't>",
                 autoWidth: false,
+                language: {
+                    lengthMenu: "Display _MENU_ of <b>_TOTAL_RECORDS_</b> partners",
+                }
             });
             initialized = true;
         }
