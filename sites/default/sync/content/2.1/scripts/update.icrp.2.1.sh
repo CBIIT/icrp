7cb00dd5e8bdf28d788a89f59c693a5a30bab37f
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2018-03-29 23:36:41 -0400

    New content scripts to help add or insert nodes.

---
+++ sites/default/sync/content/2.1/scripts/update.icrp.2.1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#SYNTAX: drush php-script update.node.drush [update | create] <node type> <node title> <file name with new HTML>
+drush php-script icrp-node.drush update page "About Us" about.us.html
