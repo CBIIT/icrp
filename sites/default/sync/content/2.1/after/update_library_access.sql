7f8a98ffa03e609b9c4255856d1195283e7288bc
Brian Park <park-brian@users.noreply.github.com>
2018-04-09 17:57:42 -0400

    do not hardcode database name

--- sites/default/sync/content/2.1/after/update_library_access.sql
+++ sites/default/sync/content/2.1/after/update_library_access.sql
@@ -3,17 +3,17 @@
 --
 INSERT INTO  user__field_library_access (`bundle`,`deleted`,`entity_id`,`revision_id`,`langcode`,`delta`,`field_library_access_value`)
 SELECT DISTINCT ur.bundle, ur.deleted, ur.entity_id, ur.revision_id, 'en', 0, 'general' FROM users u
-JOIN icrp.user__roles ur ON u.uid = ur.entity_id;
+JOIN user__roles ur ON u.uid = ur.entity_id;
 
 --
 -- Insert User library access - manager, administrator
 --
 INSERT INTO  user__field_library_access (`bundle`,`deleted`,`entity_id`,`revision_id`,`langcode`,`delta`,`field_library_access_value`)
 SELECT DISTINCT ur.bundle, ur.deleted, ur.entity_id, ur.revision_id, ur.langcode, 1, 'finance' FROM users u
-JOIN icrp.user__roles ur ON u.uid = ur.entity_id
+JOIN user__roles ur ON u.uid = ur.entity_id
 WHERE ur.roles_target_id in ('manager', 'administrator');
 
 INSERT INTO  user__field_library_access (`bundle`,`deleted`,`entity_id`,`revision_id`,`langcode`,`delta`,`field_library_access_value`)
 SELECT DISTINCT ur.bundle, ur.deleted, ur.entity_id, ur.revision_id, ur.langcode, 2, 'operations_and_contracts' FROM users u
-JOIN icrp.user__roles ur ON u.uid = ur.entity_id
+JOIN user__roles ur ON u.uid = ur.entity_id
 WHERE ur.roles_target_id in ('manager', 'administrator');
