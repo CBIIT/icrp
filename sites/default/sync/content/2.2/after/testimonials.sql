041b1e87496d1ca93f72a63f605efe5bbf3a829d
kailingchen <kailingmd@yahoo.com>
2018-08-13 12:34:25 -0400

    add missing ;

--- sites/default/sync/content/2.2/after/testimonials.sql
+++ sites/default/sync/content/2.2/after/testimonials.sql
@@ -28,14 +28,14 @@ INSERT INTO node__body (bundle, deleted, entity_id, revision_id, langcode, delta
 INSERT INTO node_revision__body (bundle, deleted, entity_id, revision_id, langcode, delta, body_value, body_summary, body_format)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'<p>“Less than an hour since arriving at #ICRP2018 annual meeting and have already had a breakfast convo about a potential international collaboration. Living the @icrpartners1 mission!”<br />\r\n&nbsp;</p>\r\n','','basic_html');
 
-INSERT INTO 'node__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'@DrHelenRippon (Worldwide Cancer Research)');
 
-INSERT INTO 'node_revision__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node_revision__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'@DrHelenRippon (Worldwide Cancer Research)');
 
-INSERT INTO 'node_access' (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
-	VALUES (@entity_id,'en',1,0,'all',1,0,0)
+INSERT INTO node_access (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
+	VALUES (@entity_id,'en',1,0,'all',1,0,0);
 
 -- Insert testimonial 2
 
@@ -65,14 +65,14 @@ INSERT INTO node__body (bundle, deleted, entity_id, revision_id, langcode, delta
 INSERT INTO node_revision__body (bundle, deleted, entity_id, revision_id, langcode, delta, body_value, body_summary, body_format)
   VALUES   ('testimonial',0,@entity_id, @revision_id,'en',0,'<p>“ICRP’s Environmental Influences on Breast Cancer report highlighted that investment in this area was declining. Over the time period studied, the numbers of active awards declined and funding levels also fell, both in absolute terms, and as a percentage of the overall breast cancer portfolio. Using these data, and other evidence, CBCRP was able to promote additional research and capacity building in this area.”<br />\r\n&nbsp;</p>\r\n','','basic_html');
 
-INSERT INTO 'node__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'Dr Mhel Kavanaugh-Lynch, California Breast Cancer Research Program');
 
-INSERT INTO 'node_revision__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node_revision__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'Dr Mhel Kavanaugh-Lynch, California Breast Cancer Research Program');
 
-INSERT INTO 'node_access' (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
-	VALUES (@entity_id,'en',1,0,'all',1,0,0)
+INSERT INTO node_access (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
+	VALUES (@entity_id,'en',1,0,'all',1,0,0);
 
 -- Insert testimonial 3
 
@@ -102,14 +102,14 @@ INSERT INTO node__body (bundle, deleted, entity_id, revision_id, langcode, delta
 INSERT INTO node_revision__body (bundle, deleted, entity_id, revision_id, langcode, delta, body_value, body_summary, body_format)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'<p>“ICRP worked with the Metastatic Breast Cancer Alliance to provide data for a landscape analysis of metastatic breast cancer research, helping the Alliance realize it’s vision of improving outcomes for women with metastatic breast cancer”<br />\r\n&nbsp;</p>\r\n','','basic_html');
 
-INSERT INTO 'node__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'Dr Marc Hurlbert, Metastatic Breast Cancer Alliance');
 
-INSERT INTO 'node_revision__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node_revision__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'Dr Marc Hurlbert, Metastatic Breast Cancer Alliance');
 
-INSERT INTO 'node_access' (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
-	VALUES (@entity_id,'en',1,0,'all',1,0,0)
+INSERT INTO node_access (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
+	VALUES (@entity_id,'en',1,0,'all',1,0,0);
 
 -- Insert testimonial 4
 
@@ -139,11 +139,11 @@ INSERT INTO node__body (bundle, deleted, entity_id, revision_id, langcode, delta
 INSERT INTO node_revision__body (bundle, deleted, entity_id, revision_id, langcode, delta, body_value, body_summary, body_format)
   VALUES   ('testimonial',0,@entity_id, @revision_id,'en',0,'<p>“Analysis of the national portfolio using the ICRP’s CSO coding system highlighted low investment in prevention research in the UK in 2004. As a result of the analysis, UK health research funders collaborated to fund over £34m and over 70 projects in prevention research to boost research capacity in this area.”<br />\r\n&nbsp;</p>\r\n','','basic_html');
 
-INSERT INTO 'node__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'UK National Cancer Research Institute');
 
-INSERT INTO 'node_revision__field_source' (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
+INSERT INTO node_revision__field_source (bundle, deleted, entity_id, revision_id, langcode, delta, field_source_value)
   VALUES ('testimonial',0,@entity_id, @revision_id,'en',0,'UK National Cancer Research Institute');
 
-INSERT INTO 'node_access' (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
-	VALUES (@entity_id,'en',1,0,'all',1,0,0)
+INSERT INTO node_access (nid, langcode, fallback, gid, realm, grant_view, grant_update, grant_delete)
+	VALUES (@entity_id,'en',1,0,'all',1,0,0);
