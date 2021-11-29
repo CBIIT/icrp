6716cc1134374924e46917a460733f39e493b1b8
Brian Park <park-brian@users.noreply.github.com>
2021-11-29 17:10:27 -0500

    use looser website validation

--- modules/custom/db_admin/src/Forms/src/app/partners/partners-form/partners-form.component.ts
+++ modules/custom/db_admin/src/Forms/src/app/partners/partners-form/partners-form.component.ts
@@ -123,7 +123,7 @@ CREATE  PROCEDURE [dbo].[UpdateNonPartner]
 
       description: [null, [Validators.required]],
       sponsorCode: [null, [Validators.required, Validators.maxLength(50)]],
-      website: [null, [Validators.maxLength(250), Validators.pattern(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i)]],
+      website: [null, [Validators.maxLength(250)]],
 
       latitude: [null, [Validators.required, Validators.min(-90), Validators.max(90)]],
       longitude: [null, [Validators.required, Validators.min(-180), Validators.max(180)]],
